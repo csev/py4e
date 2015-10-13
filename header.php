47ea114544114cc1571df9f24f5c1d1802bd25da
Charles Severance <csev@umich.edu>
2015-10-13 10:54:33 -0400

    Undo the use of dr-chuck.net

--- header.php
+++ header.php
@@ -3,6 +3,8 @@
 <link rel="alternate" type="application/rss+xml" title="PythonLearn Podcast" 
 href="http://www.pythonlearn.com/audiofeed.xml" />
 <?php
+/*
     if ( isset($_SERVER['HTTP_HOST']) && strpos($_SERVER['HTTP_HOST'], 'www.pythonlearn.com') !== false ) {
         echo('<base href="http://www.dr-chuck.net/pythonlearn/">'."\n");
     }
+*/
