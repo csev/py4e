52520d36b64b16f30eef6d7dfdb560b7d324d237
Charles Severance <csev@umich.edu>
2015-10-20 08:06:56 -1000

    Add comments about BeautifulSoup

--- code/wikigrade.py
+++ code/wikigrade.py
@@ -1,3 +1,7 @@
+# Note - this code must run in Python 2.x and you must download
+# http://www.pythonlearn.com/code/BeautifulSoup.py
+# Into the same folder as this program
+
 import string
 import sqlite3
 import urllib
