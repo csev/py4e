e8f50325c4724df7acf88b565da7556dd52950c8
Charles Severance <csev@umich.edu>
2015-11-24 12:42:48 -0500

    Tweak

--- code/emaildb.py
+++ code/emaildb.py
@@ -17,12 +17,13 @@ for line in fh:
     pieces = line.split()
     email = pieces[1]
     cur.execute('SELECT count FROM Counts WHERE email = ? ', (email, ))
-    try:
-        count = cur.fetchone()[0]
-        cur.execute('UPDATE Counts SET count=count+1 WHERE email = ?', (email, ))
-    except:
+    row = cur.fetchone()
+    if row is None:
         cur.execute('''INSERT INTO Counts (email, count) 
                 VALUES ( ?, 1 )''', ( email, ) )
+    else : 
+        cur.execute('UPDATE Counts SET count=count+1 WHERE email = ?', 
+            (email, ))
     conn.commit()
 
 # https://www.sqlite.org/lang_select.html
