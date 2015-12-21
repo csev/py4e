66becef4ee35d18f0ef93ba7c68992a602f7c9a9
Charles Severance <csev@umich.edu>
2015-12-20 23:52:59 -0500

    Some tweaks.

--- book/mgrep.py
+++ book/mgrep.py
@@ -3,7 +3,7 @@ for fn in *.mkd; do
     echo "the next file is $fn"
     x=`basename $fn .mkd`
     echo $x
-    sed 's/www.py4inf.com/www.pythonlearn.com/'g < $x.mkd > /tmp/$x.mkd
+    sed 's"www.pythonlearn.com/code/"www.pythonlearn.com/code3/"'g < $x.mkd > /tmp/$x.mkd
     cp /tmp/$x.mkd $x.mkd
 
 done
