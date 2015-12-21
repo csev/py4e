8fcf2aa14af612a7f8561bb6ec42ffd961e64783
Charles Severance <csev@umich.edu>
2015-12-21 00:11:54 -0500

    Tweaks

--- book/book.sh
+++ book/book.sh
@@ -6,7 +6,8 @@ pandoc --default-image-extension=svg --epub-stylesheet=epub.css *.mkd -o x.epub
 rm tmp.* *.tmp *.aux
 pandoc A0-preface.mkd -o tmp.prefacex.tex
 sed < tmp.prefacex.tex 's/section{/section*{/' > tmp.preface.tex
-pandoc -s -N -f markdown+definition_lists -t latex --toc --default-image-extension=eps -V fontfamily:arev -V fontsize:10pt -V documentclass:book --template=template.latex [0-9]*.mkd [A][A-Z]*.mkd -o tmp.tex
+# pandoc -s -N -f markdown+definition_lists -t latex --toc --default-image-extension=eps -V fontfamily:arev -V fontsize:10pt -V documentclass:book --template=template.latex [0-9]*.mkd [A][A-Z]*.mkd -o tmp.tex
+pandoc -s -N -f markdown+definition_lists -t latex --toc --default-image-extension=eps -V fontsize:10pt -V documentclass:book --template=template.latex [0-9]*.mkd [A][A-Z]*.mkd -o tmp.tex
 
 sed < tmp.tex '/includegraphics/s/jpg/eps/' | sed 's"includegraphics{../photos"includegraphics[height=3.0in]{../photos"' > tmp.sed
 diff tmp.sed tmp.tex
@@ -25,4 +26,4 @@ else
   echo "Output on x.pdf"
 fi
 
-rm tmp.*
+# rm tmp.*
