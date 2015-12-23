7a82617c19b1128be0f71575afed5f86a523d9fa
togar-nk <naveent@usa.net>
2015-12-23 21:49:26 +0530

    Added a check for empty arrays
    Added a check for empty arrays

--- code3/search8.py
+++ code3/search8.py
@@ -2,5 +2,7 @@ fhand = open('mbox-short.txt')
 count = 0
 for line in fhand:
     words = line.split()
-    if words[0] != 'From' : continue
-    print(words[2])
+    if len(words) > 0:
+        if words[0] != 'From':
+            continue
+        print(words[2])
--- code3/search9.py
+++ code3/search9.py
@@ -3,5 +3,7 @@ count = 0
 for line in fhand:
     words = line.split()
     print('Debug:', words)
-    if words[0] != 'From' : continue
-    print(words[2])
+    if len(words) > 0:
+        if words[0] != 'From':
+            continue
+        print(words[2])
