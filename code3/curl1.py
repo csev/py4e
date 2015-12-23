07c352a554a35361ff2ed495144b3b394e36d8f1
KumarNT100 <naveen@openridge.com>
2015-12-23 12:24:20 +0530

    added binary option to file open mode
    added binary option to file open mode while writing image files

--- code3/curl1.py
+++ code3/curl1.py
@@ -1,6 +1,6 @@
 import urllib.request, urllib.parse, urllib.error
 
 img = urllib.request.urlopen('http://www.py4inf.com/cover.jpg').read()
-fhand = open('cover.jpg', 'w')
+fhand = open('cover.jpg', 'wb')
 fhand.write(img)
 fhand.close()
