6adff547b71bcc7181d06448cb124dfb67e4e3f6
togar-nk <naveent@usa.net>
2015-12-24 08:16:31 +0530

    Added encode function call
    Added encode function call to string parameters

--- code3/socket1.py
+++ code3/socket1.py
@@ -2,7 +2,7 @@ import socket
 
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mysock.connect(('www.py4inf.com', 80))
-mysock.send('GET http://www.py4inf.com/code/romeo.txt HTTP/1.0\n\n')
+mysock.send('GET http://www.py4inf.com/code/romeo.txt HTTP/1.0\n\n'.encode())
 
 while True:
     data = mysock.recv(512)
