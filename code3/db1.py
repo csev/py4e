06ac38f19a0f78cc03c12f133ae45e667c51317b
ghfudemytraining <ghfudemytraining@s118781813.websitehome.co.uk>
2015-12-29 00:28:53 +0000

    Update db1.py

--- code3/db1.py
+++ code3/db1.py
@@ -3,7 +3,7 @@ import sqlite3
 conn = sqlite3.connect('music.sqlite')
 cur = conn.cursor()
 
-cur.execute('DROP TABLE IF EXISTS Tracks ')
+cur.execute('DROP TABLE IF EXISTS Tracks')
 cur.execute('CREATE TABLE Tracks (title TEXT, plays INTEGER)')
 
 conn.close()
