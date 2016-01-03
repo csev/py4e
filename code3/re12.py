dd5d8456d24adb72c0204f13749a9607a6f70f7b
Dustin F <thewhowhatwhere@users.noreply.github.com>
2016-01-03 12:27:24 -0800

    Update re12.py
    update comment

--- code3/re12.py
+++ code3/re12.py
@@ -1,4 +1,5 @@
-# Search for lines that start with From and have an at sign
+# Search for lines that start with From and a character followed by a two digit number between 00 and 99 followed by ':'
+# Then print the number if it is greater than zero
 import re
 hand = open('mbox-short.txt')
 for line in hand:
