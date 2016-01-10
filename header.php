2f73eab5716566af75fea0512c0ce15a8886e988
Dustin Farley <fdustin45@gmail.com>
2016-01-09 17:09:47 -0800

    1. fixed iphone issues / added meta tags 2. fixed css issues caused by iphone fix

--- header.php
+++ header.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
         <title>PythonLearn - Exploring Data</title>
-        
+        <meta charset="utf-8"> 
+        <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+        <meta name="viewport" content="width=device-width,initial-scale=1">  
         <link rel="stylesheet" href="assets/foundation/css/foundation.css" type="text/css"/>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="assets/style.css" type="text/css" />
@@ -18,13 +20,13 @@
             jQuery(window).scroll(function(){
                 var w = jQuery(window);
 
-                if (w.scrollTop() < lastScroll) {
+                if (w.scrollTop() < lastScroll || w.scrollTop() == 0) {
                     jQuery(".title-bar").show();
-                    lastScroll = w.scrollTop()+20;
+                    lastScroll = w.scrollTop()+15;
                 }
                 if (w.scrollTop() > lastScroll){
                     jQuery(".title-bar").hide();
-                    lastScroll = w.scrollTop()-20;	
+                    lastScroll = w.scrollTop()-10;	
                 }
             });
         </script>
