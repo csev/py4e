d7fc29cd165b256e664a96b8f35ab3f93ab3f8c3
Charles Severance <csev@umich.edu>
2016-01-11 16:22:52 -0500

    Add pointer to all sample code included using verbatim.py

--- book/verbatim.py
+++ book/verbatim.py
@@ -12,8 +12,16 @@ while True:
         print line
         continue
 
-    fh = open(x[0])
+    fn = x[0]
+    fh = open(fn)
     print '~~~~ {.python}'
+    blank = True
     for ln in fh:
         print ln.strip()
+        blank = len(ln.strip()) > 0 
+
+    if fn.startswith('../') :
+        if blank : print 
+        fu = fn.replace('../','http://www.pythonlearn.com/')
+        print '# Code:', fu
     print '~~~~'
