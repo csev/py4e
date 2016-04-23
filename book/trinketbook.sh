ad76dbe31c2a143bc2f361c3d2772399ca725c14
Elliott Hauser <eah13@mac.com>
2016-04-23 17:35:38 +0000

    add initial nunjucks version for testing

---
+++ book/trinketbook.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Testfile
+#cat trinket/test | python verbatim.py --trinket | pandoc -s -f markdown -t html --template=trinket/template --toc --default-image-extension=svg --css=../trinket/simplegrid.css -o html/test.html 
+
+# Clean directory
+find trinket/pfe/. -type f -not -name 'figs2' | xargs rm
+
+# Make a symlink to figs
+#(cd html && ln -s ../figs2/)
+
+# Convert all mkd into html
+for fn in *.mkd; do
+    echo "the next file is $fn"
+    x=`basename $fn .mkd`
+    echo $x
+    cat $fn | \
+    python pre-html.py | \
+    tee tmp.html.pre.$x | \
+    python verbatim.py --trinket --files | \
+    tee tmp.html.verbatim.$x | \
+    python consoles.py | \
+    pandoc -s --self-contained \
+    -f markdown -t html \
+    --template=trinket/nunjucks \
+    --toc \
+    --default-image-extension=svg \
+    -o trinket/pfe/$x.html
+done
+
+rm tmp.*
