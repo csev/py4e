8c94309ce9e2b51c3798e17079cd183e0cd02f4c
Chris Kolosiwsky <ckolos@gmail.com>
2016-04-30 15:19:30 -0500

    fixes pep8 E231 and W291

--- code3/urlwords.py
+++ code3/urlwords.py
@@ -5,5 +5,5 @@ fhand = urllib.request.urlopen('http://www.pythonlearn.com/code3/romeo.txt')
 for line in fhand:
     words = line.decode().split()
     for word in words:
-        counts[word] = counts.get(word,0) + 1   
+        counts[word] = counts.get(word, 0) + 1
 print(counts)
