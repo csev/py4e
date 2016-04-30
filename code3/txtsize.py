48244f0a07bd3df4a483e69489fe1ddfcade12d5
Chris Kolosiwsky <ckolos@gmail.com>
2016-04-30 15:12:07 -0500

    fixes pep8 E203 and E231

--- code3/txtsize.py
+++ code3/txtsize.py
@@ -2,6 +2,6 @@ import os
 from os.path import join
 for (dirname, dirs, files) in os.walk('.'):
     for filename in files:
-        if filename.endswith('.txt') :
-            thefile = os.path.join(dirname,filename)
+        if filename.endswith('.txt'):
+            thefile = os.path.join(dirname, filename)
             print(os.path.getsize(thefile), thefile)
