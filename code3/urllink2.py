bf1c99573f5b8ea401207fa4afaafe1838faa813
Chris Kolosiwsky <ckolos@gmail.com>
2016-04-30 15:15:35 -0500

    fixes pep8 E231 and W291

--- code3/urllink2.py
+++ code3/urllink2.py
@@ -1,4 +1,4 @@
-# To run this, you can install BeautifulSoup 
+# To run this, you can install BeautifulSoup
 # https://pypi.python.org/pypi/beautifulsoup4
 
 # Or download the file
@@ -12,15 +12,15 @@ url = input('Enter - ')
 html = urlopen(url).read()
 
 # html.parser is the HTML parser included in the standard Python 3 library.
-# information on other HTML parsers is here: 
-#http://www.crummy.com/software/BeautifulSoup/bs4/doc/#installing-a-parser
+# information on other HTML parsers is here:
+# http://www.crummy.com/software/BeautifulSoup/bs4/doc/#installing-a-parser
 soup = BeautifulSoup(html, "html.parser")
 
 # Retrieve all of the anchor tags
 tags = soup('a')
 for tag in tags:
     # Look at the parts of a tag
-    print('TAG:',tag)
-    print('URL:',tag.get('href', None))
-    print('Contents:',tag.contents[0])
-    print('Attrs:',tag.attrs)
+    print('TAG:', tag)
+    print('URL:', tag.get('href', None))
+    print('Contents:', tag.contents[0])
+    print('Attrs:', tag.attrs)
