21b51f37e451332b0194f1092cfa65f07f554b67
leerssej <jens.leerssen@gmail.com>
2016-05-14 23:10:38 -0700

    Revert "Whitespace stewardship: Trimming & PEP 8 Alignment"
    This reverts commit 5d6c49c2db37ae36f3df1b46bb7ef4b7f13d2c88.

--- code3/geodata/geoload.py
+++ code3/geodata/geoload.py
@@ -22,40 +22,39 @@ CREATE TABLE IF NOT EXISTS Locations (address TEXT, geodata TEXT)''')
 fh = open("where.data")
 count = 0
 for line in fh:
-    if count > 200:
-        break
+    if count > 200 : break
     address = line.strip()
     print('')
     cur.execute("SELECT geodata FROM Locations WHERE address= ?", (memoryview(address.encode()), ))
 
     try:
         data = cur.fetchone()[0]
-        print("Found in database ", address)
+        print("Found in database ",address)
         continue
     except:
         pass
 
     print('Resolving', address)
-    url = serviceurl + urllib.parse.urlencode({"sensor": "false", "address": address})
+    url = serviceurl + urllib.parse.urlencode({"sensor":"false", "address": address})
     print('Retrieving', url)
     uh = urllib.request.urlopen(url)
     data = uh.read().decode()
-    print('Retrieved', len(data), 'characters', data[:20].replace('\n', ' '))
+    print('Retrieved',len(data),'characters',data[:20].replace('\n',' '))
     count = count + 1
-    try:
+    try: 
         js = json.loads(str(data))
         print(js)  # We print in case unicode causes an error
-    except:
+    except: 
         continue
 
-    if 'status' not in js or (js['status'] != 'OK' and js['status'] != 'ZERO_RESULTS'):
+    if 'status' not in js or (js['status'] != 'OK' and js['status'] != 'ZERO_RESULTS') : 
         print('==== Failure To Retrieve ====')
         print(data)
         break
 
-    cur.execute('''INSERT INTO Locations (address, geodata)
-            VALUES ( ?, ? )''', (memoryview(address.encode()), memoryview(data.encode())))
-    conn.commit()
+    cur.execute('''INSERT INTO Locations (address, geodata) 
+            VALUES ( ?, ? )''', (memoryview(address.encode()), memoryview(data.encode()) ) )
+    conn.commit() 
     time.sleep(1)
 
 print("Run geodump.py to read the data from the database so you can vizualize it on a map.")
