01970b585405ecfd98bd47eef59d42698681313f
Charles Severance <csev@umich.edu>
2016-08-31 09:59:56 -0400

    Tweak index.php

--- index.php
+++ index.php
@@ -12,15 +12,8 @@ require_once "nav.php";
 <p>The goal of this site is to provide a set of materials in support of my <a href="book.php">Python for Everybody</a> book to allow you to learn Python on your own. This page serves as an outline of the materials to support the textbook.  
 </p>
 <p>
-If you log in to this site, you can submit homework to its autograders and track your progress through the materials.  There is a feature in development to recognize progress using "Open Badges".
-</p>
-<h2>Audio Archive</h2>
-<p>
-Here is an archive of the <a href="https://archive.org/details/201509UMSI502Podcasts_201601" target="_blank">live lecture recordings</a> from SI502 at the UM School of Information Fall 2015.
-</p>
-<p>
-Here are <a href="videos/" target="_blank">archive copies</a> of the various audio, video, PowerPoints, PDFs and closed caption files if you want to reuse these materials in your own courses. All this material (including audio and video) is
-Copyright Creative Commons Attribution 3.0 unless otherwise indicated.
+If you log in to this site, you can submit homework to its autograders and track your progress through the materials.  
+There are a number of badges that you can earn by doing the assignments in this class.
 </p>
 <h2>Under Construction</h2>
 <p>
@@ -30,6 +23,14 @@ The slides, videos, and autograders are in the process of being updated from Pyt
 <p>
 You may also be interested in the 
 older version of this book: <a href="http://www.py4inf.com" target="_blank">Python for Informatics: Exploring Information in Python 2.0</a>.
+</p>
+<h2>Audio Archive</h2>
+<p>
+Here is an archive of the <a href="https://archive.org/details/201509UMSI502Podcasts_201601" target="_blank">live lecture recordings</a> from SI502 at the UM School of Information Fall 2015.
+</p>
+<p>
+Here are <a href="videos/" target="_blank">archive copies</a> of the various audio, video, PowerPoints, PDFs and closed caption files if you want to reuse these materials in your own courses. All this material (including audio and video) is
+Copyright Creative Commons Attribution 3.0 unless otherwise indicated.
 </p>
 <!--
 <?php
