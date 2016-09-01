5f72d0dc94d89cf7020773180c0e480402389fd2
Charles Severance <csev@umich.edu>
2016-09-01 11:53:37 -0400

    Tweak the UI

--- index.php
+++ index.php
@@ -11,18 +11,20 @@ require_once "nav.php";
 <p>
 Welcome to my Massive Open Online Course (MOOC) version of Python for Everybody. 
 Now that you have logged in, you have access to course-style features of this web site.
-<ul>
-<li>
+<ul class="list-group">
+<li class="list-group-item">
 As you go through the <a href="tsugi/lessons.php">Lessons</a> in the course you now will see additional
 links to the autograders in the class.  You can attempt the autograders and get a score.</li>
-<li>
+<li class="list-group-item">
 You can track your progress through the course using the <a href="tsugi/assignments.php">Assignments</a>
 tool and when you complete a group of assignments, you can earn a <a href="tsugi/badges.php">Badge</a>.
 You can download these badges and host them on your web site or refer the badge URLs on this site.</li>
-<li>There is an
+<li class="list-group-item">
+There is an
 <a href="https://disqus.com/home/channel/pythonforeverybody/" target="_blank">online disucssion forum</a>
 hosted by Disqus.</li>
-<li>If you want to use these Creative Commons Licensed materials in your own classes you can download
+<li class="list-group-item">
+If you want to use these Creative Commons Licensed materials in your own classes you can download
 or link to the artifacts,
 <a href="tsugi/cc/export.php">export the course material</a> as an
 <a href="https://www.imsglobal.org/cc/index.html" target="_blank">
@@ -36,24 +38,28 @@ Hello and welcome to my site where you can work through my course materials rela
 my free <a href="book.php">Python for Everybody</a> text book.
 <p>
 You can use this web site many different ways:
-<ul>
-<li>You browse my videos and course materials under <a href="tsugi/lessons.php">Lessons</a>.  The materials
+<ul class="list-group">
+<li class="list-group-item">
+You browse my videos and course materials under <a href="tsugi/lessons.php">Lessons</a>.  The materials
 I have developed
 for this class are all provided with a Creative Commons license so you can download or link to
 them to incorporate them into your own teaching if you like.</li>
-<li>This site uses <a href="http://www.tsugi.org" target="_blank">Tsugi</a> framework to embed a learning
+<li class="list-group-item">
+This site uses <a href="http://www.tsugi.org" target="_blank">Tsugi</a> framework to embed a learning
 management system into this site.  So if you <a href="tsugi/login.php">log in</a> to this site
 it is as if you have joined a free, global
 open and online course.  You have a grade book, autograded assignments, a discussion forum, and can earn
 badges for your efforts.</li>
-<li> This site supports the <a href="https://www.imsglobal.org/cc/index.html" target="_blank">
+<li class="list-group-item">
+This site supports the <a href="https://www.imsglobal.org/cc/index.html" target="_blank">
 IMS Common Cartridge® Specification</a> that allows you to extract all of the material from the course
 and import it into a Learning Management system like
 <a href="http://www.sakaiproject.org" target="_blank">Sakai</a>, Moodle, Canvas, Blackboaard, BrightSpace, or others.
 You can download a cartridge using the <a href="tsugi/cc/export.php">export feature</a>.  You will also need
 an <a href="tsugi/admin/key/index.php">IMS LTI key and secret</a> to launch the autograders from your LMS.
 </li>
-<li>The code for this site including the autograders, slides, and course content is all available on
+<li class="list-group-item">
+The code for this site including the autograders, slides, and course content is all available on
 <a href="https://github.com/csev/pythonlearn" target="_blank">GitHub</a>.  That means you could make your own
 copy of the course site, publish it and remix it any way you like.  Even more exciting, you could translate
 the entire site (course) into your own language and publish it.</li>
