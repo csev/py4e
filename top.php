1c82742ffc165eabb7222ccec5b15cfa2a52d29d
Charles Severance <csev@umich.edu>
2016-09-15 20:53:43 -0400

    Tweak header and footer

--- top.php
+++ top.php
@@ -5,4 +5,20 @@ define('COOKIE_SESSION', true);
 require_once "tsugi/config.php";
 $LAUNCH = LTIX::session_start();
 
+// https://lefkomedia.com/adding-external-link-indicator-with-css/
+$head = '
+<style>
+a[target="_blank"]:after {
+    font-family: "FontAwesome";
+    content: " \f08e";
+}
+.goog-te-banner-frame.skiptranslate {
+    display: none !important;
+    } 
+body {
+    top: 0px !important; 
+    }
+</style>';
+
+$OUTPUT->setAppHeader($head);
 $OUTPUT->header();
