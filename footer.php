39aae0e6cd313410a4207d36d3183b9a12b9c0ed
Charles Severance <csev@umich.edu>
2016-09-15 21:01:06 -0400

    Moar progress

--- footer.php
+++ footer.php
@@ -1,15 +1,6 @@
 <?php
 
-$foot = '';
-if ( ! $CFG->OFFLINE ) $foot .= '
-<div id="google_translate_element" style="position: fixed; right: 1em; bottom: 0.25em;"></div><script type="text/javascript">
-function googleTranslateElementInit() {
-  new google.translate.TranslateElement({pageLanguage: "en", layout: google.translate.TranslateElement.InlineLayout.SIMPLE, gaTrack: true, gaId: "UA-423997-22"}, "google_translate_element");
-}
-</script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
-';
-
-$foot .= '
+$foot = '
 <p style="font-size: 75%; margin-top: 5em;">
 Copyright Creative Commons Attribution 3.0 - Charles R. Severance
 </p>
--- top.php
+++ top.php
@@ -5,20 +5,4 @@ define('COOKIE_SESSION', true);
 require_once "tsugi/config.php";
 $LAUNCH = LTIX::session_start();
 
-// https://lefkomedia.com/adding-external-link-indicator-with-css/
-$head = '
-<style>
-a[target="_blank"]:after {
-    font-family: "FontAwesome";
-    content: " \f08e";
-}
-.goog-te-banner-frame.skiptranslate {
-    display: none !important;
-    } 
-body {
-    top: 0px !important; 
-    }
-</style>';
-
-$OUTPUT->setAppHeader($head);
 $OUTPUT->header();
