cfbeb95f393f0605015bb4d3be1d9cc20b452135
Charles Severance <csev@umich.edu>
2016-09-28 17:13:01 -0400

    Tweaks

--- code3/words.py
+++ code3/words.py
@@ -1,10 +1,11 @@
 name = input('Enter file:')
 handle = open(name, 'r')
-text = handle.read()
-words = text.split()
 counts = dict()
-for word in words:
-    counts[word] = counts.get(word, 0) + 1
+
+for line in handle:
+    words = line.split()
+    for word in words:
+        counts[word] = counts.get(word, 0) + 1
 
 bigcount = None
 bigword = None
