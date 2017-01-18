e5e291f3a8efeef657356d1ae1dbd912490b53a1
Charles Severance <csev@umich.edu>
2017-01-18 11:00:12 -0500

    Activate the Map!

--- nav.php
+++ nav.php
@@ -20,7 +20,7 @@ if ( isset($_SESSION['id']) ) {
 if ( isset($_SESSION['id']) ) {
     $submenu = new \Tsugi\UI\Menu();
     $submenu->addLink('Profile', $T.'profile.php');
-    if ( isset($CFG->google_map_api_key) && $adminmenu ) {
+    if ( isset($CFG->google_map_api_key) ) {
         $submenu->addLink('Map', $T.'map.php');
     }
     $submenu->addLink('Badges', $T.'badges.php');
