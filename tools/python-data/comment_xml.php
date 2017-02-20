3938734ce38efa33255cd088877869e4da8bb982
Charles Severance <csev@umich.edu>
2017-02-20 16:19:28 -0500

    Tweaks.

--- tools/python-data/comment_xml.php
+++ tools/python-data/comment_xml.php
@@ -111,7 +111,7 @@ node and then loop through the child nodes of the comments node.
 $ python solution.py 
 Enter location: http://py4e-data.dr-chuck.net/comments_42.xml
 Retrieving http://py4e-data.dr-chuck.net/comments_42.xml
-Retrieved 4204 characters
+Retrieved 4189 characters
 Count: 50
 Sum: 2...
 </pre>
