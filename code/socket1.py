cd5f7a490f920246a187c596bfbfbc51dfcbd8e5
Charles Severance <csev@umich.edu>
2017-05-12 16:31:33 +0200

    Fix Sample Code

--- code/socket1.py
+++ code/socket1.py
@@ -1,8 +1,8 @@
 import socket
 
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mysock.connect(('www.py4inf.com', 80))
-mysock.send('GET http://www.py4inf.com/code/romeo.txt HTTP/1.0\n\n')
+mysock.connect(('data.pr4e.org', 80))
+mysock.send('GET http://data.pr4e.org/romeo.txt HTTP/1.0\n\n')
 
 while True:
     data = mysock.recv(512)
