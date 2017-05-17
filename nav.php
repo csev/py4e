21836b1fe2d2d5809dc77d49a394675c77fac51f
Charles Severance <csev@umich.edu>
2017-05-17 07:55:05 -0600

    Use the REST Urls

--- nav.php
+++ nav.php
@@ -18,10 +18,10 @@ if ( isset($_SESSION['id']) ) {
     if ( $P7 ) {
         $set->addLeft('Assignments', $R.'assignments');
     } else {
-        $set->addLeft('Assignments', $T.'assignments.php');
+        $set->addLeft('Assignments', $T.'assignments');
     }
     // If both are set we go to discuss.php
-    if ( isset($CFG->disqushost) ) $set->addLeft('Discuss', $T.'discuss.php');
+    if ( isset($CFG->disqushost) ) $set->addLeft('Discuss', $T.'discuss');
     else if ( isset($CFG->disquschannel) ) $set->addLeft('Discuss', $CFG->disquschannel);
 } else {
     $set->addLeft('Materials', $R.'materials');
@@ -32,27 +32,27 @@ if ( isset($_SESSION['id']) ) {
     if ( $P7 ) {
         $submenu->addLink('Profile', $R.'profile');
     } else {
-        $submenu->addLink('Profile', $T.'profile.php');
+        $submenu->addLink('Profile', $T.'profile');
     }
     if ( isset($CFG->google_map_api_key) ) {
         if ( $P7 ) {
             $submenu->addLink('Map', $R.'map');
         } else {
-            $submenu->addLink('Map', $T.'map.php');
+            $submenu->addLink('Map', $T.'map');
         }
     }
 
     if ( $P7 ) {
         $submenu->addLink('Badges', $R.'badges');
     } else {
-        $submenu->addLink('Badges', $T.'badges.php');
+        $submenu->addLink('Badges', $T.'badges');
     }
     $submenu->addLink('Materials', $R.'materials');
     if ( $CFG->DEVELOPER ) {
-        $submenu->addLink('Test LTI Tools', $T . 'dev.php');
+        $submenu->addLink('Test LTI Tools', $T . 'dev');
     }
     if ( $CFG->providekeys ) {
-        $submenu->addLink('LMS Integration', $T . 'admin/key/index.php');
+        $submenu->addLink('LMS Integration', $T . 'admin/key/indexp');
     }
     if ( isset($_COOKIE['adminmenu']) && $_COOKIE['adminmenu'] == "true" ) {
         $submenu->addLink('Administer', $T . 'admin/');
@@ -61,7 +61,7 @@ if ( isset($_SESSION['id']) ) {
     if ( $P7 ) {
         $submenu->addLink('Logout', $R.'logout');
     } else {
-        $submenu->addLink('Logout', $T.'logout.php');
+        $submenu->addLink('Logout', $T.'logout');
     }
     if ( isset($_SESSION['avatar']) ) {
         $set->addRight('<img src="'.$_SESSION['avatar'].'" style="height: 2em;"/>', $submenu);
@@ -73,7 +73,7 @@ if ( isset($_SESSION['id']) ) {
     if ( $P7 ) {
         $set->addRight('Login', $R.'login');
     } else {
-        $set->addRight('Login', $T.'login.php');
+        $set->addRight('Login', $T.'login');
     }
 }
 
