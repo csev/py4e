4b1d4f4cbbd6a09070042579e54fdbf77e466428
Charles Severance <csev@umich.edu>
2017-06-21 12:49:00 -0400

    Add PY3 example

--- tools/python-data/regex_sum.php
+++ tools/python-data/regex_sum.php
@@ -138,8 +138,13 @@ There are a number of different ways to approach this problem.  While we don't r
 to write the most compact code possible, it can sometimes be a fun exercise.  Here is a 
 a redacted version of two-line version of this program using list comprehension:
 <pre>
+Python 2
 import re
 print sum( [ ****** *** * in **********('[0-9]+',**************************.read()) ] )
+
+Python 3:
+import re
+print( sum( [ ****** *** * in **********('[0-9]+',**************************.read()) ] ) )
 </pre>
 Please don't waste a lot of time trying to figure out the shortest solution until you 
 have completed the homework.   List comprehension is mentioned in Chapter 10 and the 
