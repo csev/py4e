7f4569b9cdda193bb64f1eddfb04e54a536a28bf
Charles Severance <csev@umich.edu>
2017-06-25 10:16:22 -0400

    From Mihaela

--- code3/socket1.py
+++ code3/socket1.py
@@ -2,7 +2,7 @@ import socket
 
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mysock.connect(('data.pr4e.org', 80))
-cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\n\n'.encode()
+cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
 mysock.send(cmd)
 
 while True:
