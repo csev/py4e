7c443d514dead097c527e258a77aa9f3d36ddfe2
Charles Severance <csev@umich.edu>
2017-06-25 12:49:41 -0400

    Push back

--- code3/socket1.py
+++ code3/socket1.py
@@ -5,7 +5,6 @@ mysock.connect(('data.pr4e.org', 80))
 cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
 mysock.send(cmd)
 
-
 while True:
     data = mysock.recv(512)
     if (len(data) < 1):
