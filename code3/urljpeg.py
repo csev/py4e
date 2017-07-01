e7a457155cd0f977da81b5ff7acc1c918c8a99e2
Charles Severance <csev@umich.edu>
2017-07-01 09:38:24 -0400

    Fix from tamara

--- code3/urljpeg.py
+++ code3/urljpeg.py
@@ -5,7 +5,7 @@ HOST = 'data.pr4e.org'
 PORT = 80
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mysock.connect((HOST, PORT))
-mysock.sendall(b'GET http://data.pr4e.org/cover3.jpg HTTP/1.0\n\n')
+mysock.sendall(b'GET http://data.pr4e.org/cover3.jpg HTTP/1.0\r\n\r\n')
 count = 0
 picture = b""
 
