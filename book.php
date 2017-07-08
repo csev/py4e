f3d4e0454fdf5f0155e4e32cf968bebfc2314787
Charles Severance <csev@umich.edu>
2017-07-08 10:49:56 -0400

    Fix up those URLs.

--- book.php
+++ book.php
@@ -19,7 +19,7 @@ of the book has not been translated.
   <li>
      English
      <ul class="menu vertical nested is-active">
-       <li class="menu-text"><a href="html3">Online Book</a></li>
+       <li class="menu-text"><a href="html3">Online HTML Book</a></li>
        <li class="menu-text">Printed Book on <a href="http://amzn.to/29w0lsX" target="_blank">Amazon</a></li>
        <li class="menu-text"><a href="http://amzn.to/29LrtC0" target="_blank">Kindle edition</a> of the book</li>
        <li class="menu-text"><a href="trinket3" target="_blank">Interactive HTML from Trinket.io</a></li>
--- bookfoot.php
+++ bookfoot.php
@@ -31,22 +31,22 @@ function onSelect() {
 </script>    
 <div style="float:right">
 <select id="chapters" onchange="onSelect();">
-  <option <?= x_sel("01-intro.php") ?>>Chapter 1: Introduction</option>
-  <option <?= x_sel("02-variables.php") ?>>Chapter 2: Variables</option>
-  <option <?= x_sel("03-conditional.php") ?>>Chapter 3: Conditionals</option>
-  <option <?= x_sel("04-functions.php") ?>>Chapter 4: Functions</option>
-  <option <?= x_sel("05-iterations.php") ?>>Chapter 5: Iterations</option>
-  <option <?= x_sel("06-strings.php") ?>>Chapter 6: Strings</option>
-  <option <?= x_sel("07-files.php") ?>>Chapter 7: Files</option>
-  <option <?= x_sel("08-lists.php") ?>>Chapter 8: Lists</option>
-  <option <?= x_sel("09-dictionaries.php") ?>>Chapter 9: Dictionaries</option>
-  <option <?= x_sel("10-tuples.php") ?>>Chapter 10: Tuples</option>
-  <option <?= x_sel("11-regex.php") ?>>Chapter 11: Regex</option>
-  <option <?= x_sel("12-network.php") ?>>Chapter 12: Networked Programs</option>
-  <option <?= x_sel("13-web.php") ?>>Chapter 13: Python and Web Services</option>
-  <option <?= x_sel("14-objects.php") ?>>Chapter 14: Python Objects</option>
-  <option <?= x_sel("15-database.php") ?>>Chapter 15: Python and Databases</option>
-  <option <?= x_sel("16-viz.php") ?>>Chapter 16: Data Vizualization</option>
+  <option <?= x_sel("01-intro") ?>>Chapter 1: Introduction</option>
+  <option <?= x_sel("02-variables") ?>>Chapter 2: Variables</option>
+  <option <?= x_sel("03-conditional") ?>>Chapter 3: Conditionals</option>
+  <option <?= x_sel("04-functions") ?>>Chapter 4: Functions</option>
+  <option <?= x_sel("05-iterations") ?>>Chapter 5: Iterations</option>
+  <option <?= x_sel("06-strings") ?>>Chapter 6: Strings</option>
+  <option <?= x_sel("07-files") ?>>Chapter 7: Files</option>
+  <option <?= x_sel("08-lists") ?>>Chapter 8: Lists</option>
+  <option <?= x_sel("09-dictionaries") ?>>Chapter 9: Dictionaries</option>
+  <option <?= x_sel("10-tuples") ?>>Chapter 10: Tuples</option>
+  <option <?= x_sel("11-regex") ?>>Chapter 11: Regex</option>
+  <option <?= x_sel("12-network") ?>>Chapter 12: Networked Programs</option>
+  <option <?= x_sel("13-web") ?>>Chapter 13: Python and Web Services</option>
+  <option <?= x_sel("14-objects") ?>>Chapter 14: Python Objects</option>
+  <option <?= x_sel("15-database") ?>>Chapter 15: Python and Databases</option>
+  <option <?= x_sel("16-viz") ?>>Chapter 16: Data Vizualization</option>
 </select>
 </div>
 
