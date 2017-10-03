e9b62ca9be388ff5bf2c180f4e75e50c374f9607
Charles Severance <csev@umich.edu>
2017-10-03 12:21:23 -0400

    Add email example.

--- code3/emaildb.py
+++ code3/emaildb.py
@@ -3,8 +3,7 @@ import sqlite3
 conn = sqlite3.connect('emaildb.sqlite')
 cur = conn.cursor()
 
-cur.execute('''
-DROP TABLE IF EXISTS Counts''')
+cur.execute('DROP TABLE IF EXISTS Counts')
 
 cur.execute('''
 CREATE TABLE Counts (email TEXT, count INTEGER)''')
