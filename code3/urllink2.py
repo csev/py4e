350565572bb98393497488dc5e554dc201c197ed
TheGNUdist <joshua.g.coyne@gmail.com>
2018-04-19 16:58:27 -0500

    Remove BS4 link
    I removed the link to installing a parser from the comments
    and placed that text in ch 12.

--- code3/urllink2.py
+++ code3/urllink2.py
@@ -5,7 +5,6 @@
 # http://www.py4e.com/code3/bs4.zip
 # and unzip it in the same directory as this file
 
-
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import ssl
@@ -17,10 +16,6 @@ ctx.verify_mode = ssl.CERT_NONE
 
 url = input('Enter - ')
 html = urlopen(url, context=ctx).read()
-
-# html.parser is the HTML parser included in the standard Python 3 library.
-# information on other HTML parsers is here:
-# http://www.crummy.com/software/BeautifulSoup/bs4/doc/#installing-a-parser
 soup = BeautifulSoup(html, "html.parser")
 
 # Retrieve all of the anchor tags
