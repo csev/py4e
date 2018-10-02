30df61dcac27eefc2b225bcd2939f967018c97af
Charles Severance <csev@umich.edu>
2018-10-02 14:32:40 -0400

    Support XML

--- tools/python-data/data/geocode.php
+++ tools/python-data/data/geocode.php
@@ -8,11 +8,18 @@ if ( ! isset($_GET['address']) ) {
     die("No address...");
 }
 
+$do_json = strpos($_REQUEST_URI,'geocode/xml') === false;
+$fragment = $do_json ? 'json' : 'xml';
+
 $address = $_GET['address'];
 
-$serviceurl = "https://maps.googleapis.com/maps/api/geocode/json?key=".$CFG->GEOCODE_KEY."&address=";
+$serviceurl = "https://maps.googleapis.com/maps/api/geocode/$fragment?key=".$CFG->GEOCODE_KEY."&address=";
 
-header('Content-Type: application/json');
+if ( $do_json ) {
+    header('Content-Type: application/json');
+} else {
+    header('Content-Type: application/xml');
+}
 
 $url = $serviceurl . urlencode($address);
 error_log("geocode ".$address);
