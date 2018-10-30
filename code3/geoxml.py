9f3d68d350dcf0388b49e4692986bb7df476057c
Charles Severance <csev@umich.edu>
2018-10-29 23:40:15 -0400

    Add documentation

--- code3/geoxml.py
+++ code3/geoxml.py
@@ -5,6 +5,7 @@ import ssl
 api_key = False
 # If you have a Google Places API key, enter it here
 # api_key = 'AIzaSy___IDByT70'
+# https://developers.google.com/maps/documentation/geocoding/intro
 
 if api_key is False:
     api_key = 42
