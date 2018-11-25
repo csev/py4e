90b1796ca79b4d5ed96fc5a097c4490913210c7d
Charles Severance <csev@umich.edu>
2018-11-25 10:49:36 -0500

    Update the fallback patterns.

--- tools/sql-intro/tsugi.php
+++ tools/sql-intro/tsugi.php
@@ -2,19 +2,5 @@
 
 require_once('../config.php');
 
-// Make PHP paths pretty .../install => install.php
-$router = new Tsugi\Util\FileRouter();
-$file = $router->fileCheck();
-if ( $file ) {
-    require_once($file);
-    return;
-}
-
-// Make a Tsugi Application
-$launch = \Tsugi\Core\LTIX::requireData();
-$app = new \Tsugi\Silex\Application($launch);
-
-// Add some routes
-\Tsugi\Controllers\Analytics::routes($app);
-
-$app->run();
+$tool = new \Tsugi\Core\Tool();
+$tool->run();
