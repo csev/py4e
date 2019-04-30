096550545ab05e4546c94c1f20aa9dce5f7331a7
Charles Severance <csev@umich.edu>
2019-04-30 08:57:41 -0400

    Updates

--- book.php
+++ book.php
@@ -22,9 +22,9 @@ The <a href="http://www.py4inf.com/">Python 2 version</a> of the book is still a
    target="_blank">Shroff Publishing</a>)
 </li>
        <li class="menu-text"><a href="http://amzn.to/29LrtC0" target="_blank">Kindle edition</a> of the book</li>
-       <li class="menu-text"><a href="html3">Online HTML Book</a></li>
-       <li class="menu-text"><a href="http://do1.dr-chuck.com/pythonlearn/EN_us/pythonlearn.pdf" target="_blank">Download PDF</a></li>
-       <li class="menu-text"><a href="http://do1.dr-chuck.com/pythonlearn/EN_us/pythonlearn.epub" target="_blank">Download EPUB</a></li>
+       <li class="menu-text">Free: <a href="http://do1.dr-chuck.com/pythonlearn/EN_us/pythonlearn.pdf" target="_blank">PDF</a>,
+       <a href="html3">HTML</a>,
+       <a href="http://do1.dr-chuck.com/pythonlearn/EN_us/pythonlearn.epub" target="_blank">EPUB</a></li>
        <li class="menu-text"><a href="https://eng.libretexts.org/Textbook_Maps/Computer_Science/Map%3A_Python_for_Everybody_(Severance)" target="_blank">HTML with examples in Jupyter Notebooks from LibreTexts.org</a></li>
 <!--
        <li class="menu-text"><a href="https://github.com/csev/py4e/raw/master/book3/zips/pfe.zip" target="_blank">Download HTML in a ZIP (~6MB)</a></li>
@@ -38,46 +38,48 @@ The <a href="http://www.py4inf.com/">Python 2 version</a> of the book is still a
   <li> Italian
 <a href="https://amzn.to/2qGksK6" target="_blank">Python per tutti: Esplorare dati con Python3</a>
      <ul class="menu vertical nested is-active">
-       <li class="menu-text">Printed book on <a href="https://amzn.to/2qGksK6" target="_blank">Amazon</a></li>
-       <li class="menu-text"><a href="http://do1.dr-chuck.com/pythonlearn/IT_it/pythonlearn.pdf" target="_blank">Download PDF</a></li>
-       <li class="menu-text"><a href="http://do1.dr-chuck.com/pythonlearn/IT_it/pythonlearn.epub" target="_blank">Download EPUB</a></li>
-<li> Translation team:
+       <li class="menu-text">Printed book on <a href="https://amzn.to/2qGksK6" target="_blank">Amazon</a>
+       (Free <a href="http://do1.dr-chuck.com/pythonlearn/IT_it/pythonlearn.pdf" target="_blank">PDF</a>,
+       <a href="http://do1.dr-chuck.com/pythonlearn/IT_it/pythonlearn.epub" target="_blank">EPUB</a>)
+</li>
+<li> 
+<a href="`https://github.com/vittore/py4e/" target="_blank">Book source on github</a>, 
+(Thanks to
 <a href="https://www.linkedin.com/in/alessandrorossetti/" target="_blank">Alessandro Rossetti</a>
 and
-<a href="https://www.linkedin.com/in/vittorezen/" target="_blank">Vittore Zen</a>
-(<a href="https://twitter.com/vittore" target="_blank">Twitter</a>,
-<a href="https://github.com/vittore" target="_blank">Github</a>).
+<a href="https://www.linkedin.com/in/vittorezen/" target="_blank">Vittore Zen</a>)
 </li>
      </ul>
   </li>
   <li> Portuguese - In progress
      <ul class="menu vertical nested is-active">
-       <li class="menu-text"><a href="http://do1.dr-chuck.com/pythonlearn/PT_br/pythonlearn.pdf" target="_blank">Download PDF</a> (Partially translated)</li>
-       <li class="menu-text"><a href="http://do1.dr-chuck.com/pythonlearn/PT_br/pythonlearn.epub" target="_blank">Download EPUB</a> (Partially translated)</li>
+       <li class="menu-text">Free:
+        <a href="http://do1.dr-chuck.com/pythonlearn/PT_br/pythonlearn.pdf" target="_blank">PDF</a>,
+       <a href="http://do1.dr-chuck.com/pythonlearn/PT_br/pythonlearn.epub" target="_blank">EPUB</a>
+        </li>
         <li> <a href="https://github.com/ras-ufcg/py4e" target="_blank">Book source on github</a> 
 (Thanks to Yuri Loia de Medeiros).
 </li>
 
-        <li>Translation by:
-            <a href="https://github.com/ftardio/pythonlearn" target="_blank">Fernando Tardio</a>.
-        </li>
      </ul>
   </li>
   <li> Spanish - In progress
      <ul class="menu vertical nested is-active">
-       <li class="menu-text"><a href="http://do1.dr-chuck.com/pythonlearn/ES_es/pythonlearn.pdf" target="_blank">Download PDF</a> (Partially translated)</li>
-       <li class="menu-text"><a href="http://do1.dr-chuck.com/pythonlearn/ES_es/pythonlearn.epub" target="_blank">Download EPUB</a> (Partially translated)</li>
-        <li>Translation by:
-            <a href="https://github.com/ftardio/pythonlearn" target="_blank">Fernando Tardio</a>.
+       <li class="menu-text">Free: <a href="http://do1.dr-chuck.com/pythonlearn/ES_es/pythonlearn.pdf" target="_blank">PDF</a>,
+       <a href="http://do1.dr-chuck.com/pythonlearn/ES_es/pythonlearn.epub" target="_blank">EPUB</a></li>
+        <li>
+            <a href="https://github.com/ftardio/pythonlearn" target="_blank">Book source on github</a>
+(Thanks to Fernando Tardio)
         </li>
      </ul>
   </li>
 <li>
 <p>
 If you are insterested in starting a translation of the book, I have some
-instructions for getting started in
+instructions for 
 <a href="https://github.com/csev/py4e/blob/master/TRANSLATION.md"
- target="_blank">in github</a>.
+ target="_blank">
+getting started</a>.
 </p>
 </li>
 
