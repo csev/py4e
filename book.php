ff1a16a50fdae4245b1a54aeaf33105e80133741
Charles Severance <csev@umich.edu>
2019-07-05 13:32:16 -0400

    Change the list

--- book.php
+++ book.php
@@ -19,71 +19,9 @@
     </p>
   </li>
   <li>
-     English
-     <ul class="menu vertical nested is-active">
-       <li class="menu-text">Printed book on <a href="http://amzn.to/29w0lsX" target="_blank">Amazon</a></li>
-       <li class="menu-text">Printed book on
-<a href="https://www.amazon.in/dp/9352136276" target="_blank">Amazon India</a>
-(low-cost shipping within India thanks to
-<a href="http://www.shroffpublishers.com/books/9789352136278/"
-   target="_blank">Shroff Publishing</a>)
-</li>
-       <li class="menu-text"><a href="http://amzn.to/29LrtC0" target="_blank">Kindle edition</a> of the book</li>
-       <li class="menu-text">Free: <a href="http://do1.dr-chuck.com/pythonlearn/EN_us/pythonlearn.pdf" target="_blank">PDF</a>,
-       <a href="http://do1.dr-chuck.com/pythonlearn/EN_us/pythonlearn.epub" target="_blank">EPUB</a></li>
-       <li class="menu-text"><a href="https://eng.libretexts.org/Textbook_Maps/Computer_Science/Map%3A_Python_for_Everybody_(Severance)" target="_blank">HTML with examples in Jupyter Notebooks from LibreTexts.org</a></li>
-<!--
-       <li class="menu-text"><a href="https://github.com/csev/py4e/raw/master/book3/zips/pfe.zip" target="_blank">Download HTML in a ZIP (~6MB)</a></li>
--->
-<!--
-       <li class="menu-text"><a href="trinket3" target="_blank">Interactive HTML from Trinket.io</a></li>
--->
-       <li class="menu-text"><a href="https://books.trinket.io/pfe/index.html" target="_blank">Interactive HTML from Trinket.io</a></li>
-     </ul>
+     <a href="https://www.py4e.com/book" target="_blank">Other Languages</a>
   </li>
-  <li> Italian
-<a href="https://amzn.to/2qGksK6" target="_blank">Python per tutti: Esplorare dati con Python3</a>
-     <ul class="menu vertical nested is-active">
-       <li class="menu-text">Printed book on <a href="https://amzn.to/2qGksK6" target="_blank">Amazon</a>
-       (Free <a href="http://do1.dr-chuck.com/pythonlearn/IT_it/pythonlearn.pdf" target="_blank">PDF</a>,
-       <a href="http://do1.dr-chuck.com/pythonlearn/IT_it/pythonlearn.epub" target="_blank">EPUB</a>)
-</li>
-<li> 
-<a href="`https://github.com/vittore/py4e/" target="_blank">Book source on github</a>, 
-(Thanks to
-<a href="https://www.linkedin.com/in/alessandrorossetti/" target="_blank">Alessandro Rossetti</a>
-and
-<a href="https://www.linkedin.com/in/vittorezen/" target="_blank">Vittore Zen</a>)
-</li>
-     </ul>
-  </li>
-  <li> Portuguese - In progress
-     <ul class="menu vertical nested is-active">
-       <li class="menu-text">Free:
-        <a href="http://do1.dr-chuck.com/pythonlearn/PT_br/pythonlearn.pdf" target="_blank">PDF</a>,
-       <a href="http://do1.dr-chuck.com/pythonlearn/PT_br/pythonlearn.epub" target="_blank">EPUB</a>
-        </li>
-        <li> <a href="https://github.com/ras-ufcg/py4e" target="_blank">Book source on github</a> 
-(Thanks to Yuri Loia de Medeiros).
-</li>
-     </ul>
-  </li>
-  <li>
-     Chinese - There is not yet a translation for the book, but if you are in China and have
-trouble viewing the videos there is a copy available at
-<a href="https://www.bilibili.com/video/av46649799" target="_new">
-https://www.bilibili.com/video/av46649799</a> that work well within China.
-  </li>
-
 </ul>
-
-<p>
-If you are insterested in starting a translation of the book, I have some
-instructions for 
-<a href="https://github.com/csev/py4e/blob/master/TRANSLATION.md"
- target="_blank">
-getting started</a>.
-</p>
 
         <p>
             The sample code and data files for the book is here: <a href="http://www.py4e.com/code3/" target="_blank">Code Samples</a>.
