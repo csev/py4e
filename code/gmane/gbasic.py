5ab1a7d93b2f75c3d92a7e0abc808e00f0122654
srcatto <srcatto@support10.uk>
2019-07-21 20:35:47 +0100

    Update gbasic.py
    For the people still using the Python v2 code, add in the data confirmation line that exists in the v3 code.
    Loaded messages= n subjects= n senders= n
    Downloaded https://www.py4e.com/code/gmane.zip , ran it several times, made the edit and tested again. Erase files, unzip for clean start, edit gbasic.py again and tested. 
    
    Getting correct output, checked against input dicts, 
    >py -2 gbasic.py
    How many to dump? 11
    Loaded messages= 25 subjects= 19 senders= 19
    
    Top 11 Email list participants
    ys2n@virginia.edu 2
    
    tested using python.org distribution
    Python 2.7.12 (v2.7.12:d33e0cf91556, Jun 27 2016, 15:24:40) [MSC v.1500 64 bit (AMD64)] on win32
    
    Top 11 Email list organizations
    cam.ac.uk 3
    
    C:\py4e\py2 source\gmane>

--- code/gmane/gbasic.py
+++ code/gmane/gbasic.py
@@ -9,6 +9,24 @@ conn = sqlite3.connect('index.sqlite')
 conn.text_factory = str
 cur = conn.cursor()
 
+cur.execute('SELECT id, sender FROM Senders')
+senders = dict()
+for message_row in cur :
+    senders[message_row[0]] = message_row[1]
+
+cur.execute('SELECT id, subject FROM Subjects')
+subjects = dict()
+for message_row in cur :
+    subjects[message_row[0]] = message_row[1]
+
+# cur.execute('SELECT id, guid,sender_id,subject_id,headers,body FROM Messages')
+cur.execute('SELECT id, guid,sender_id,subject_id,sent_at FROM Messages')
+messages = dict()
+for message_row in cur :
+    messages[message_row[0]] = (message_row[1],message_row[2],message_row[3],message_row[4])
+
+print 'Loaded messages=',len(messages),'subjects=',len(subjects),'senders=',len(senders)
+
 cur.execute('''SELECT Messages.id, sender FROM Messages
     JOIN Senders ON Messages.sender_id = Senders.id''')
     
