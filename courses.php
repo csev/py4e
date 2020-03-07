78ea39d938c86d1359a7b7813810e0d97cf73f51
Juan Dougnac <juanillo58@hotmail.com>
2020-03-07 19:01:45 -0300

    Update courses.php
    Translated to Spanish. 
    If there are courses in English using the book, it might be appropriate to include it here.

--- courses.php
+++ courses.php
@@ -1,46 +1,47 @@
 <?php include("top.php"); ?>
 <?php include("nav.php");?>
 <div style="float: right;"/><iframe style="padding: 10px; width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ss&ref=as_ss_li_til&ad_type=product_link&tracking_id=drchu02-20&marketplace=amazon&region=US&placement=1530051126&asins=1530051126&linkId=2ea6c883c6cf11f29568856139bad34b&show_border=true&link_opens_in_new_window=true"></iframe></div>
-        <h2>Courses using Python for Everybody</h2>
+        <h2>Cursos que usan Python Para Todos</h2>
         <p>
-This page reflects various courses that use this textbook around the world.
-This translation / adaptation / reuse is encouraged since the materials are
-avilable under a Creative Commons license.
+
+Esta página muestra varios cursos alrededor del mundo que usan este libro.
+Esta traducción/ adaptación/ reutilización es alentada, ya que los materiales
+están disponibles bajo una licencia de Creative Commons.                
         </p>
 
 <ul>
-<li><p><b>Earn a Coursera Certificate for This Course</b>
-<a href="https://www.coursera.org/specializations/python" target="_blank">Python for Everybody</a> (Coursera Specialization)</li>
+<li><p><b>Obtén un certificado Coursera con este curso</b>
+<a href="https://www.coursera.org/specializations/python" target="_blank">Python Para Todos</a> (Especialización de Coursera)</li>
 <ul>
 <li>
 <a href="https://www.coursera.org/learn/python-ar" target="_blank">
 البرمجة للجميع - بدء استخدام بايثون
-</a> (Arabic version)
+</a> (Versión en Árabe)
 </li>
 </ul>
 </p>
 </li>
-<li><p><b>Earn an edX Certificate for This Course</b>
-<a href="https://www.edx.org/course/programming-for-everybody-getting-started-with-python" target="_blank">Python for Everybody - Getting Started</a> (Two edX courses)</li>
+<li><p><b>Obtén un certificado de edX con este curso</b>
+<a href="https://www.edx.org/course/programming-for-everybody-getting-started-with-python" target="_blank">Python Para Todos - Introducción</a> (dos cursos edX)</li>
 <p></li>
 <p>
 <li>
 <a href="https://www.freecodecamp.org/news/python-for-everybody/" target="_blank">FreeCodeCamp</a> 
 (<a href="https://www.youtube.com/watch?v=8DvywoWv6fI" target="_blank">Video</a>)
-- Blends online courses with meetups around the world.
+- Mezcla cursos en línea con encuentros en persona alrededor del mundo.
 </li>
 </p>
 <li>
 <p>
 <a href="https://edufyre.com/p/programming-made-easy-with-python"
-target="_blank">Programming Made Easy with Python</a> from 
+target="_blank">Programming Made Easy with Python</a> de 
 <a href="https://edufyre.com/" target="_blank">Edufyre</a> (India)
 </p>
 <li><p>
 <a href="http://www.edwith.org/pythonforeverybody" target="_blank">
-모두를 위한 프로그래밍 : 파이썬 (Programming for All: Python)</a>
-from 
-<a href="http://www.edwith.org/" target="_blank">Edwith</a> (Korean).
+모두를 위한 프로그래밍 : 파이썬 (Programación Para Todos: Python)</a>
+por
+<a href="http://www.edwith.org/" target="_blank">Edwith</a> (Coreano).
 </p></li>
 <li><p>
 <a href="https://videodirect.amazon.com/content/edit/season?id=4XS87SAAA29"
