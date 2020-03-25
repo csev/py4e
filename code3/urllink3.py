b50c25714b1fa3df299af8943ced2dd174b3bac2
Juan Carlos Perez Castellanos <cuyopc@gmail.com>
2020-03-25 09:55:51 -0600

    Translate urllib scripts to Spanish
    Translation of Python scripts used in chapter 12
    
    Signed-off-by: Juan Carlos Perez Castellanos <cuyopc@gmail.com>

--- code3/urllink3.py
+++ code3/urllink3.py
@@ -1,32 +1,32 @@
-# To run this, you can install BeautifulSoup
+# Para ejecutar este programa descarga BeautifulSoup
 # https://pypi.python.org/pypi/beautifulsoup4
 
-# Or download the file
+# O descarga el archivo
 # http://www.py4e.com/code3/bs4.zip
-# and unzip it in the same directory as this file
+# y descomprimelo en el mismo directorio que este archivo
 
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
 import ssl
 
-# Ignore SSL certificate errors for https
+# Ignorar errores de certificado SSL
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-todo = list()
-visited = list()
-url = input('Enter - ')
-todo.append(url)
-count = int(input('How many to retrieve - '))
+pendientes = list()
+visitados = list()
+url = input('Introduzca - ')
+pendientes.append(url)
+contador = int(input('Cuántos para recuperar - '))
 
-while len(todo) > 0 and count > 0 :
-    print("====== To Retrieve:",count, "Queue Length:", len(todo))
-    url = todo.pop()
-    count = count - 1
+while len(pendientes) > 0 and contador > 0 :
+    print("====== Por Recuperar:",contador, "En Espera:", len(pendientes))
+    url = pendientes.pop()
+    contador = contador - 1
 
     if (not url.startswith('http')):
-        print("Skipping", url)
+        print("Ignorando", url)
         continue
 
     if (url.find('facebook') > 0):
@@ -35,24 +35,24 @@ while len(todo) > 0 and count > 0 :
     if (url.find('linkedin') > 0):
         continue
 
-    if (url in visited):
-        print("Visited", url)
+    if (url in visitados):
+        print("Visitado", url)
         continue
 
-    print("===== Retrieving ", url)
+    print("===== Recuperando ", url)
 
     try:
         html = urllib.request.urlopen(url, context=ctx).read()
     except:
-        print("*** Error in retrieval")
+        print("*** Error en la recuperación")
         continue
 
     soup = BeautifulSoup(html, 'html.parser')
-    visited.append(url)
+    visitados.append(url)
 
-    # Retrieve all of the anchor tags
-    tags = soup('a')
-    for tag in tags:
-        newurl = tag.get('href', None)
-        if (newurl is not None):
-            todo.append(newurl)
+    # Recuperar todos las etiquetas de anclaje
+    etiquetas = soup('a')
+    for etiqueta in etiquetas:
+        nuevaurl = etiqueta.get('href', None)
+        if (nuevaurl is not None):
+            pendientes.append(nuevaurl)
