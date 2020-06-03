e5459a7f81a8af1f1ab95be588887f1d0c5b9960
Charles Severance <csev@umich.edu>
2020-06-03 14:54:14 -0400

    Add deprecation messages - remove Python 2.x unit tests

---
+++ lectures/index.php
@@ -0,0 +1,21 @@
+<h1>Python 2.x is Deprecated</h2>
+<p>
+Hello - as of January 1, 2020, Python 2.x is no longer officially
+supported.   This folder on the Python for Everybody web site
+is old Python 2.x versions of some of the materials. You should
+visit the Python 3.x versions of these materials.
+</p>
+<p>
+<a href="../lectures3">Python 3.x Lectures</a>
+</p>
+<p>
+If you want to see the Python 2.x lectures, will be available in the
+Python for Everybody github repo:
+</p>
+<p>
+<a href="https://github.com/csev/py4e/tree/master/code">Github</a>
+</p>
+<p>
+This Python 2.x material will be deleted from the github repository
+on or after January 1, 2021.
+</p>
