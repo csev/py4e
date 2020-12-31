b481cf8e883e592c75f30010aef9edd78d5721ff
Charles Severance <csev@umich.edu>
2020-12-31 10:57:57 -0500

    Make errors prettier

--- launcherror.php
+++ launcherror.php
@@ -3,6 +3,13 @@ use \Tsugi\Util\U;
 
 $error = U::get($_REQUEST, 'detail');
 
+// Adjust / internationalize
+if ( strpos($error,"Session expired - please re-launch") === 0 ||
+    strpos($error,"Session has expired") === 0 ) {
+
+    $error = __('Your session has expired');
+}
+
 $_SESSION['error'] = $error;
 
 header("Location: ".addSession($CFG->apphome));
