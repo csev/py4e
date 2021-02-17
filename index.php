d2350c6dada438fd946ab0a1f2a7334b53ddece8
Charles Severance <csev@umich.edu>
2021-02-17 17:42:16 -0500

    Add new opening video.

--- index.php
+++ index.php
@@ -5,20 +5,24 @@ use \Tsugi\UI\Output;
 require_once "top.php";
 require_once "nav.php";
 ?>
+<div id="container">
+<div style="margin-left: 10px; float:right">
+<iframe width="400" height="225" src="https://www.youtube.com/embed/UjeNA_JtXME?rel=0" frameborder="0" allowfullscreen></iframe>
+</div>
 <h1>Python for Everybody</h1>
 <?php if ( isset($_SESSION['id']) ) { ?>
 <p>
 Welcome to my Massive Open Online Course (MOOC) version of Python for Everybody. 
 Now that you have logged in, you have access to course-style features of this web site.
-<ul class="list-group">
-<li class="list-group-item">
+<ul>
+<li>
 As you go through the <a href="lessons">Lessons</a> in the course you now will see additional
 links to the autograders in the class.  You can attempt the autograders and get a score.</li>
-<li class="list-group-item">
+<li>
 You can track your progress through the course using the <a href="assignments">Assignments</a>
 tool and when you complete a group of assignments, you can earn a <a href="badges">Badge</a>.
 You can download these badges and host them on your web site or refer the badge URLs on this site.</li>
-<li class="list-group-item">
+<li>
 If you want to use these Creative Commons Licensed materials 
 in your own classes you can 
 <a href="materials.php">download or link</a> to the artifacts on this site,
@@ -40,22 +44,22 @@ as the
 </p>
 <p>
 You can use this web site many different ways:
-<ul class="list-group">
-<li class="list-group-item">
+<ul>
+<li>
 You browse my videos and course materials under <a href="lessons">Lessons</a>.  The materials
 I have developed
 for this class are all provided with a Creative Commons license so you can download or link to
 them to incorporate them into your own teaching if you like.</li>
-<li class="list-group-item">
+<li>
 If you <a href="tsugi/login.php">log in</a> to this site
 it is as if you have joined a free, global
 open and online course.  You have a grade book, autograded assignments, a discussion forum, and can earn
 badges for your efforts.</li>
-<li class="list-group-item">
+<li>
 We take your privacy seriously on this site, you can review our
 <a href="privacy">Privacy Policy</a> for more details.
 </li>
-<li class="list-group-item">
+<li>
 If you want to use these materials 
 in your own classes you can download or link to the artifacts on this site,
 <a href="tsugi/cc/">export the course material</a> as an
@@ -64,7 +68,7 @@ an IMS Learning Tools Interoperability® (LTI®)
 <a href="tsugi/admin/key/index.php">key and secret</a>
  to launch the autograders from your LMS.
 </li>
-<li class="list-group-item">
+<li>
 The code for this site including the autograders, slides, and course content is all available on
 <a href="https://github.com/csev/py4e" target="_blank">GitHub</a>.  That means you could make your own
 copy of the course site, publish it and remix it any way you like.  Even more exciting, you could translate
@@ -88,4 +92,5 @@ echo(Output::safe_var_dump($_SESSION));
 var_dump($USER);
 ?>
 -->
+</div>
 <?php $OUTPUT->footer();
