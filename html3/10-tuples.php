8821ee825c76a1e8039f41361aca385b9508c6fa
srcatto <srcatto@support10.uk>
2021-06-25 00:26:15 +0100

    Update 10-tuples.php
    Same update as book3 files, pr 297 573aea5

--- html3/10-tuples.php
+++ html3/10-tuples.php
@@ -303,7 +303,7 @@ for key, val in lst[:10]:
 </dd>
 </dl>
 <h2 id="exercises">Exercises</h2>
-<p><strong>Exercise 1: Revise a previous program as follows: Read and parse the “From” lines and pull out the addresses from the line. Count the number of messages from each person using a dictionary.</strong></p>
+<p><strong>Exercise 1: Revise a previous program as follows: Read and parse the “From ” lines and pull out the addresses from the line. Count the number of messages from each person using a dictionary.</strong></p>
 <p><strong>After all the data has been read, print the person with the most commits by creating a list of (count, email) tuples from the dictionary. Then sort the list in reverse order and print out the person who has the most commits.</strong></p>
 <pre><code>Sample Line:
 From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008
@@ -313,7 +313,7 @@ cwen@iupui.edu 5
 
 Enter a file name: mbox.txt
 zqian@umich.edu 195</code></pre>
-<p><strong>Exercise 2: This program counts the distribution of the hour of the day for each of the messages. You can pull the hour from the “From” line by finding the time string and then splitting that string into parts using the colon character. Once you have accumulated the counts for each hour, print out the counts, one per line, sorted by hour as shown below.</strong></p>
+<p><strong>Exercise 2: This program counts the distribution of the hour of the day for each of the messages. You can pull the hour from the “From ” line by finding the time string and then splitting that string into parts using the colon character. Once you have accumulated the counts for each hour, print out the counts, one per line, sorted by hour as shown below.</strong></p>
 <pre><code>python timeofday.py
 Enter a file name: mbox-short.txt
 04 3
