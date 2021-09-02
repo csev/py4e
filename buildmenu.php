4fd2d1e143c29387bc1024eac6edaeb7c786edf9
konkiourtidou <dinakiourtidou1@gmail.com>
2021-09-02 18:54:19 +0300

    Update buildmenu.php

--- buildmenu.php
+++ buildmenu.php
@@ -7,37 +7,37 @@ function buildMenu() {
     $adminmenu = isset($_COOKIE['adminmenu']) && $_COOKIE['adminmenu'] == "true";
     $set = new \Tsugi\UI\MenuSet();
     $set->setHome($CFG->servicename, $CFG->apphome);
-    $set->addLeft('Lessons', $R.'lessons');
-    if ( isset($CFG->tdiscus) && $CFG->tdiscus ) $set->addLeft('Discussions', $R.'discussions');
+    $set->addLeft('Μαθήματα', $R.'lessons');
+    if ( isset($CFG->tdiscus) && $CFG->tdiscus  ) $set->addLeft('Συζητήσεις', $R.'discussions');
     if ( isset($_SESSION['id']) ) {
-        $set->addLeft('Assignments', $R.'assignments');
+        $set->addLeft('Εργασίες', $R.'assignments');
     } else {
-        $set->addLeft('OER', $R.'materials');
+        $set->addLeft('Ανοιχτοί Εκπαιδευτικοί Πόροι', $R.'materials');
     }
     
     if ( isset($_SESSION['id']) ) {
         $submenu = new \Tsugi\UI\Menu();
-        $submenu->addLink('Profile', $R.'profile');
+        $submenu->addLink('Προφίλ', $R.'profile');
         if ( isset($CFG->google_map_api_key) ) {
-            $submenu->addLink('Map', $R.'map');
+            $submenu->addLink('Χάρτης', $R.'map');
         }
     
         $submenu->addLink('Badges', $R.'badges');
-        $submenu->addLink('Materials', $R.'materials');
+        $submenu->addLink('Υλικό', $R.'materials');
         if ( $CFG->providekeys ) {
-            $submenu->addLink('LMS Integration', $T . 'settings');
+            $submenu->addLink('Ενσωμάτωση LMS', $T . 'settings');
         }
         if ( isset($CFG->google_classroom_secret) ) {
             $submenu->addLink('Google Classroom', $T.'gclass/login');
         }
         $submenu->addLink('Free App Store', 'https://www.tsugicloud.org');
-        $submenu->addLink('Rate this course', 'https://www.class-central.com/mooc/7363/python-for-everybody');
-        $submenu->addLink('Privacy', $R.'privacy');
-        $submenu->addLink('Service Level', $R.'service');
+        $submenu->addLink('Βαθμολογήστε αυτό το μάθημα', 'https://www.class-central.com/mooc/7363/python-for-everybody');
+        $submenu->addLink('Απόρρητο', $R.'privacy');
+        $submenu->addLink('επίπεδο υπηρεσιών', $R.'service');
         if ( isset($_COOKIE['adminmenu']) && $_COOKIE['adminmenu'] == "true" ) {
-            $submenu->addLink('Administer', $T . 'admin/');
+            $submenu->addLink('Διαχειριστής', $T . 'admin/');
         }
-        $submenu->addLink('Logout', $R.'logout');
+        $submenu->addLink('Αποσύνδεση', $R.'logout');
 
         if ( isset($_SESSION['avatar']) ) {
             $set->addRight('<img src="'.$_SESSION['avatar'].'" title="'.htmlentities(__('User Profile Menu - Includes logout')).'" style="height: 2em;"/>', $submenu);
@@ -46,15 +46,15 @@ function buildMenu() {
             $set->addRight(htmlentities($_SESSION['displayname']), $submenu);
         }
     } else {
-        $set->addRight('Login', $R.'login');
+        $set->addRight('Σύνδεση', $R.'login');
     }
 
     $imenu = new \Tsugi\UI\Menu();
 
-    $imenu->addLink('Instructor', 'http://www.dr-chuck.com');
+    $imenu->addLink('Εκπαιδευτής', 'http://www.dr-chuck.com');
     $imenu->addLink('Office Hours', 'http://www.dr-chuck.com/office/');
-    $set->addRight('Book', $R . 'book');
-    $set->addRight('Instructor', $imenu);
+    $set->addRight('Βιβλίο', $R . 'book');
+    $set->addRight('Εκπαιδευτής', $imenu);
 
     return($set);
 }
