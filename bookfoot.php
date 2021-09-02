0dca36cad7216065d1e44001d8adf11ed39559e2
konkiourtidou <dinakiourtidou1@gmail.com>
2021-09-02 18:52:19 +0300

    Update bookfoot.php

--- bookfoot.php
+++ bookfoot.php
@@ -31,22 +31,22 @@ function onSelect() {
 </script>    
 <div style="float:right">
 <select id="chapters" onchange="onSelect();">
-  <option <?= x_sel("01-intro") ?>>Chapter 1: Introduction</option>
-  <option <?= x_sel("02-variables") ?>>Chapter 2: Variables</option>
-  <option <?= x_sel("03-conditional") ?>>Chapter 3: Conditionals</option>
-  <option <?= x_sel("04-functions") ?>>Chapter 4: Functions</option>
-  <option <?= x_sel("05-iterations") ?>>Chapter 5: Iterations</option>
-  <option <?= x_sel("06-strings") ?>>Chapter 6: Strings</option>
-  <option <?= x_sel("07-files") ?>>Chapter 7: Files</option>
-  <option <?= x_sel("08-lists") ?>>Chapter 8: Lists</option>
-  <option <?= x_sel("09-dictionaries") ?>>Chapter 9: Dictionaries</option>
-  <option <?= x_sel("10-tuples") ?>>Chapter 10: Tuples</option>
-  <option <?= x_sel("11-regex") ?>>Chapter 11: Regex</option>
-  <option <?= x_sel("12-network") ?>>Chapter 12: Networked Programs</option>
-  <option <?= x_sel("13-web") ?>>Chapter 13: Python and Web Services</option>
-  <option <?= x_sel("14-objects") ?>>Chapter 14: Python Objects</option>
-  <option <?= x_sel("15-database") ?>>Chapter 15: Python and Databases</option>
-  <option <?= x_sel("16-viz") ?>>Chapter 16: Data Vizualization</option>
+  <option <?= x_sel("01-intro") ?>>Κεφάλαιο 1: Εισαγωγή</option>
+  <option <?= x_sel("02-variables") ?>>Κεφάλαιο 2: Μεταβλητές</option>
+  <option <?= x_sel("03-conditional") ?>>Κεφάλαιο 3: Συνθήκες</option>
+  <option <?= x_sel("04-functions") ?>>Κεφάλαιο 4: Συναρτήσεις</option>
+  <option <?= x_sel("05-iterations") ?>>Κεφάλαιο 5: Επαναλήψεις</option>
+  <option <?= x_sel("06-strings") ?>>Κεφάλαιο 6: Strings - Συμβολοσειρές</option>
+  <option <?= x_sel("07-files") ?>>Κεφάλαιο 7: Αρχεία</option>
+  <option <?= x_sel("08-lists") ?>>Κεφάλαιο 8: Λίστες</option>
+  <option <?= x_sel("09-dictionaries") ?>>Κεφάλαιο 9: Λεξικά - Dictionaries</option>
+  <option <?= x_sel("10-tuples") ?>>Κεφάλαιο 10: Πλειάδες - Tuples</option>
+  <option <?= x_sel("11-regex") ?>>Κεφάλαιο 11: Σύντομες Εκφράσεις - Regex</option>
+  <option <?= x_sel("12-network") ?>>Κεφάλαιο 12: Δικτυακά Προγράμματα</option>
+  <option <?= x_sel("13-web") ?>>Κεφάλαιο 13: Python και Υπηρεσίες Ιστού</option>
+  <option <?= x_sel("14-objects") ?>>Κεφάλαιο 14: Αντικείμενα στην Python </option>
+  <option <?= x_sel("15-database") ?>>Κεφάλαιο 15: Python και Βάσεις Δεδομένων</option>
+  <option <?= x_sel("16-viz") ?>>Κεφάλαιο 16: Οπτικοποίηση Δεδομένων</option>
 </select>
 </div>
 
@@ -55,7 +55,7 @@ echo($body);
 ?>
 <hr/>
 <p>
-If you find a mistake in this book, feel free to send me a fix using
+Αν εντοπίσετε κάποιο λάθος σε αυτό το βιβλίο μην δστάσετε να μου στείλετε τη διόρθωση στο
 <a href="https://github.com/csev/py4e/tree/master/book3" target="_blank">Github</a>.
 </p>
 <?php
