08cee57a0844116768e55e8aed5ee17cb6fec8e9
konkiourtidou <dinakiourtidou1@gmail.com>
2021-09-04 22:35:44 +0300

    Update software-win.php

--- software-win.php
+++ software-win.php
@@ -12,19 +12,19 @@ type <b>python</b> at the command line prompt to run Python.
 <b>Installing the Atom Text Editor</b>
 <p>Please download and install Atom from this site:
 </p>
-<p><a href="http://atom.io" target="_blank">http://atom.io</a>
+<p><a href="http://atom.io" target="_blank">http://atom.io</a></p>
 
 <h1>Writing a Python 3 program with Atom on Windows-10</h1>
 <p>
 We have a short 
 <a href="https://www.youtube.com/watch?v=uZbaYeYGYRQ&index=1&list=PLlRFEj9H3Oj7Bp8-DfGpfAfDBiblRfl5p" target="_blank">
 step-by-step video</a> showing how to install Python 3 and Atom and write your first program.
-<p>
+</p>
 <h1>Windows Command Line Notes</h1>
 <p>
 When the command line starts <b style="color:black;background-color:#a0ffff">up</b>, you are in your "home" directory.  Your home directory 
 is different for each of the operating systems.  
-In each of these examples, your logged in account should be used instead of csev.
+In each of these examples, your logged in account should be used instead of csev.</p>
 <pre>
     Windows XP:             C:\Documents and Settings\csev
     Windows Vista:          C:\Users\csev
@@ -36,6 +36,7 @@ The command line prompt usually includes some clue as to
 where you are in the folder structure on your hard drive.
 <p>
 If you want to really figure out where you are, use the cd command with no parameters
+</p>
 <pre>
     C:\Users\csev> cd
     C:\Users\csev
@@ -55,21 +56,22 @@ which files are in the current directory and the
 <p> 
 <b>Nifty Trick:</b>  On the cd command, you can partially type a folder name like
 Desktop and then press the TAB key and the system will auto-complete the folder name 
-if you have typed enough that the system can accurately guess what you mean to type.
+if you have typed enough that the system can accurately guess what you mean to type.</p>
 <p>
-If you get Lost...
+If you get Lost...</p>
 <p>
 If you can't figure out what folder you are in and/or cannot figure out how 
 to get to the folder you want to get to - simply close and re-open 
 the Command Line / Terminal window.  You will be back to 
-your "home" directory - so you can start from a known location.
+your "home" directory - so you can start from a known location.</p>
 <p>
 Some Cool Hints on the Windows Command Line Interface
+</p>
 <p>
 If you click on the little icon in the upper-left of the command prompt window 
 and select Preferences - you can set many things about the command line - probably 
 the most important is to set the Command History Buffer Size to be 999.
-<p>
+</p>
 <h1>Running Your Python Program in the Command Line</h1>
 <p>
 To run your program in the command line you type at the command line prompt.
