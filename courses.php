08a2bf5f2a26969e5be781f83462ce04dc36b6d9
konkiourtidou <dinakiourtidou1@gmail.com>
2021-09-05 15:44:35 +0300

    Update courses.php

--- courses.php
+++ courses.php
@@ -1,81 +1,85 @@
 <?php include("top.php"); ?>
 <?php include("nav.php");?>
 <div style="float: right; margin: 5px; "/><iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ss&ref=as_ss_li_til&ad_type=product_link&tracking_id=drchu02-20&marketplace=amazon&region=US&placement=1530051126&asins=1530051126&linkId=2ea6c883c6cf11f29568856139bad34b&show_border=true&link_opens_in_new_window=true"></iframe></div>
-<h2>Courses using Python for Everybody</h2>
+<h2>Μαθήματα που χρησιμοποιούν την Python για Όλους</h2>
 <p>
-This page reflects various courses that use this textbook around the world.
-This translation / adaptation / reuse is encouraged since the materials are
-avilable under a Creative Commons license.
+  Αυτή η σελίδα αντικατοπτρίζει διάφορα μαθήματα που χρησιμοποιούν αυτό το εγχειρίδιο σε όλο τον κόσμο.
+  Η μετάφραση / προσαρμογή / επαναχρησιμοποίησή του ενθαρρύνεται, αφού το υλικό διατίθενται υπό άδεια Creative Commons.
 </p>
-
 <ul>
-<li><p><b>Earn a Coursera Certificate for This Course</b>
-<a href="https://www.coursera.org/specializations/python" target="_blank">Python for Everybody</a> (Coursera Specialization)</li>
-<ul>
-<li>
-<a href="https://www.coursera.org/learn/python-ar" target="_blank">
-البرمجة للجميع - بدء استخدام بايثون
-</a> (Arabic version)
-</li>
+  <li>
+    <p>
+      <b>Earn a Coursera Certificate for This Course</b>
+      <a href="https://www.coursera.org/specializations/python" target="_blank">Python for Everybody</a> (Coursera Specialization)
+    </p>
+  </li>
+  <ul>
+    <li>
+      <a href="https://www.coursera.org/learn/python-ar" target="_blank"> البرمجة للجميع - بدء استخدام بايثون</a> (Αραβική έκδοση)
+    </li>
+  </ul>
+  </li>
+  <li>
+    <p>
+      <b>Earn an edX Certificate for This Course</b>
+      <a href="https://www.edx.org/course/programming-for-everybody-getting-started-with-python" target="_blank">Python for Everybody - Getting Started</a> (Δύο μαθήματα edX)
+    </p>
+  </li>
+  <li>
+    <p>
+      <b>Earn a FutureLearn Certificate for This Course</b>
+      <a href="https://www.futurelearn.com/courses/programming-for-everybody-python" target="_blank">Python for Everybody - Getting Started</a> (Δύο μαθήματα FutureLearn)
+    </p>
+  </li>
+  <li>
+    <p>
+      <a href="https://www.freecodecamp.org/news/python-for-everybody/" target="_blank">FreeCodeCamp</a> 
+      (<a href="https://www.youtube.com/watch?v=8DvywoWv6fI" target="_blank">Video</a>)
+      - Συνδυάζει διαδικτυακά μαθήματα με συναντήσεις σε όλο τον κόσμο.
+    </p>
+  </li>
+  <li>
+    <p>
+      <a href="https://edufyre.com/p/programming-made-easy-with-python" target="_blank">Programming Made Easy with Python</a> from 
+      <a href="https://edufyre.com/" target="_blank">Edufyre</a> (Ινδία)
+    </p>
+  </li>
+  <li>
+    <p>
+      <a href="http://www.edwith.org/pythonforeverybody" target="_blank">모두를 위한 프로그래밍 : 파이썬 (Programming for All: Python)</a>
+      από το <a href="https://www.edwith.org/" target="_blank">Edwith</a> (Κορέα).
+    </p>
+  </li>
+  <li>
+    <p>
+      <a href="https://training.gom.com" target="_blank">GOM Training Center</a>
+    </p>
+  </li>
+  <li>
+    <p>
+      <a href="https://videodirect.amazon.com/content/edit/season?id=4XS87SAAA29" target="_blank">Amazon Video Direct</a>.
+    </p>
+  </li>
+  <li>
+    <p>
+      <a href="https://www.codementor.io/w/coding-starter-kit?utm_source=py4e&utm_campaign=outreach" target="_blank">CodeMentor Starting Kit</a>
+      - Codementor is an on-demand mentorship marketplace for developers. Το πρόγραμμα <a href="https://www.codementor.io/students" target="_blank">
+      Codementor for Students</a> δίνει δωρεάν μονάδες για φοιτητές με διεύθυνση ηλεκτρονικού ταχυδρομείου ".edu".
+    </p>
+  </li>
+  <li>
+    <p>
+      <a href="https://www.boostcourse.org/cs122" target="_blank">모두를 위한 파이썬 (PY4E)</a>
+      Από το <a href="https://www.edwith.org/" target="_blank">Edwith</a>. (Κορέα)
+    </p>
+  </li>
+  <li>
+    <p>
+      <a href="http://steamvietnam.vn/py4e/" target="_blank">Python for Everybody</a> 
+      Από <a href="http://vng.edu.vn/" target="_blank">VNG Education</a> (Βιετνάμ)
+    </p>
+  </li>
 </ul>
-</p>
-</li>
-<li><p><b>Earn an edX Certificate for This Course</b>
-<a href="https://www.edx.org/course/programming-for-everybody-getting-started-with-python" target="_blank">Python for Everybody - Getting Started</a> (Two edX courses)</li>
-<p></li>
-<li><p><b>Earn a FutureLearn Certificate for This Course</b>
-<a href="https://www.futurelearn.com/courses/programming-for-everybody-python" target="_blank">Python for Everybody - Getting Started</a> (Two FutureLearn courses)</li>
-<p></li>
-<p>
-<li>
-<a href="https://www.freecodecamp.org/news/python-for-everybody/" target="_blank">FreeCodeCamp</a> 
-(<a href="https://www.youtube.com/watch?v=8DvywoWv6fI" target="_blank">Video</a>)
-- Blends online courses with meetups around the world.
-</li>
-</p>
-<li>
-<p>
-<a href="https://edufyre.com/p/programming-made-easy-with-python"
-target="_blank">Programming Made Easy with Python</a> from 
-<a href="https://edufyre.com/" target="_blank">Edufyre</a> (India)
-</p>
-<li><p>
-<a href="http://www.edwith.org/pythonforeverybody" target="_blank">
-모두를 위한 프로그래밍 : 파이썬 (Programming for All: Python)</a>
-from 
-<a href="https://www.edwith.org/" target="_blank">Edwith</a> (Korean).
-</p></li>
-<li><p>
-<a href="https://training.gom.com" target="_blank">
-GOM Training Center</a>
-</p></li>
-<li><p>
-<a href="https://videodirect.amazon.com/content/edit/season?id=4XS87SAAA29"
-target="_blank">Amazon Video Direct</a>.
-</p></li>
-<li><p>
-<a href="https://www.codementor.io/w/coding-starter-kit?utm_source=py4e&utm_campaign=outreach" target="_blank">
-CodeMentor Starting Kit</a> - Codementor is an on-demand 
-mentorship marketplace for developers.  The 
-<a href="https://www.codementor.io/students" target="_blank">
-Codementor for Students</a> program gives free credits for students with
-a ".edu" email address.
-</p></li>
-<li><p>
-<a href="https://www.boostcourse.org/cs122" target="_blank">
-모두를 위한 파이썬 (PY4E)</a>
-From <a href="https://www.edwith.org/" target="_blank">edwith</a>.
-(Korean)
-</p></li>
-<li><p>
-<a href="http://steamvietnam.vn/py4e/" target="_blank">
-Python for Everybody</a> 
-From <a href="http://vng.edu.vn/" target="_blank">VNG Education</a>
-(Vietnam)
-</p></li>
-</ul>
-</div>
-</div>
 <?php 
 include("footer.php"); 
 ?>
