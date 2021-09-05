0e270825c1b0eff27d39bef63207b2a49f9c8d66
konkiourtidou <dinakiourtidou1@gmail.com>
2021-09-05 15:57:15 +0300

    Update service.php

--- service.php
+++ service.php
@@ -1,13 +1,20 @@
 <?php include("top.php");?>
 <?php include("nav.php");?>
-        <h2>Terms of Service</h2>
-<p>This is a highly reliable scalable and secure cloud-based service, but outages may occur. But since there are no fees for this service, there is no formal service level agreement for this service. If you are having issues with the performance or reliability of this site, please contact us.</p>
-<p>This site cares greatly about protecting the anyone who uses these servers.  Please review our
-<a href="privacy">Privacy Policy</a> for more details.
+        <h2>Όροι χρήσης</h2>
+<p>
+        Αυτή είναι μια εξαιρετικά αξιόπιστη, κλιμακούμενη και ασφαλής υπηρεσία που βασίζεται στο cloud, αλλά ενδέχεται να προκύψουν διακοπές. Αλλά δεδομένου ότι δεν υπάρχουν
+        χρεώσεις για αυτήν την υπηρεσία, δεν υπάρχει επίσημη συμφωνία επιπέδου υπηρεσιών για αυτήν την. Εάν αντιμετωπίζετε προβλήματα με την απόδοση ή την αξιοπιστία αυτού του
+        ιστότοπου, επικοινωνήστε μαζί μας. 
 </p>
-<p><strong>Notifications</strong> </p>
-<p>In order to be in line with Fair Information Practices we will take the following responsive action, should a data breach occur: We will notify you via email</p>
-<pre><code>Python for Everybody (py4e.com)
+<p> 
+        Αυτός ο ιστότοπος ενδιαφέρεται πολύ για την προστασία όσων χρησιμοποιούν αυτούς τους διακομιστές. Ανατρέξτε στην <a href="privacy"> Πολιτική απορρήτου </a> για 
+        περισσότερες λεπτομέρειες.
+</p>
+<p><strong>Ειδοποιήσεις</strong> </p>
+<p>
+        Για να είμαστε σύμφωνοι με τις Δίκαιες Πρακτικές Πληροφόρησης, θα λάβουμε τις ακόλουθες απαντητικές ενέργειες, εάν προκύψει παραβίαση δεδομένων: Θα σας ενημερώσουμε μέσω email
+</p>
+<pre><code>Python για Όλους (gr.py4e.com)
 c/o Learning Experiences (learnxp.com)
 2190 Aurelius Rd. Unit 175
 Holt MI 48842-9998
