02c3ac4fdfd5755457962db0e5a2102fe2b62598
konkiourtidou <dinakiourtidou1@gmail.com>
2021-09-06 12:52:26 +0300

    Update book.php

--- book.php
+++ book.php
@@ -1,168 +1,150 @@
 <?php include("top.php"); ?>
 <?php include("nav.php");?>
 <div style="float: right; margin: 5px;"/><iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ss&ref=as_ss_li_til&ad_type=product_link&tracking_id=drchu02-20&marketplace=amazon&region=US&placement=1530051126&asins=1530051126&linkId=2ea6c883c6cf11f29568856139bad34b&show_border=true&link_opens_in_new_window=true"></iframe></div>
-<h2>Python for Everybody</h2>
-<p>
-The goal of this book is to provide an Informatics-oriented introduction to programming. The primary difference between a computer science approach and the Informatics approach taken in this book is a greater focus on using Python to solve data analysis problems common in the world of Informatics.
+
+<h2>Python για Όλους</h2>
+        <p>
+                Ο στόχος αυτού του βιβλίου είναι να παρέχει μια εισαγωγή στον προγραμματισμό με γνώμονα την Πληροφορική. Η κύρια διαφορά μεταξύ της προσέγγισης της επιστήμης των 
+                υπολογιστών και της προσέγγισης της Πληροφορικής που ακολουθείται σε αυτό το βιβλίο είναι η μεγαλύτερη εστίαση στη χρήση της Python για την επίλυση προβλημάτων 
+                ανάλυσης δεδομένων που είναι κοινά στον κόσμο της Πληροφορικής.
         </p>
         <p>
-            The sample code and data files for the book is here: <a href="http://www.py4e.com/code3/" target="_blank">Code Samples</a>.
+                Τα δείγματα κώδικα και αρχεία δεδομένων για το βιβλίο είναι εδώ: <a href="http://www.py4e.com/code3/" target="_blank">Δείγματα Κώδικα</a>.
         </p>
-<p><b><a href="courses">Other courses / web sites using this book</a></b>
-</p>
-<p>
-Book translations:
-</p>
-<ul>
-  <li>
-     English -
-<a href="https://amzn.to/29w0lsX" target="_blank">Python for Everbody: Exploring Data in Python3</a>
-     <ul class="menu vertical nested is-active">
-       <li class="menu-text">Printed book on
-<a href="https://www.amazon.in/dp/9352136276" target="_blank">Amazon India</a>
-(low-cost shipping within India thanks to
-<a href="http://www.shroffpublishers.com/books/9789352136278/"
-   target="_blank">Shroff Publishing</a>)
-</li>
-       <li class="menu-text"><a href="http://amzn.to/29LrtC0" target="_blank">Kindle edition</a> of the book</li>
-       <li class="menu-text">Free: <a href="http://do1.dr-chuck.com/pythonlearn/EN_us/pythonlearn.pdf" target="_blank">PDF</a>,
-       <a href="html3">HTML</a>,
-       <a href="http://do1.dr-chuck.com/pythonlearn/EN_us/pythonlearn.epub" target="_blank">EPUB</a></li>
-       <li class="menu-text"><a href="https://eng.libretexts.org/Textbook_Maps/Computer_Science/Map%3A_Python_for_Everybody_(Severance)" target="_blank">HTML with examples in Jupyter Notebooks from LibreTexts.org</a></li>
+        <p>
+                <b><a href="courses">Άλλα μαθήματα / ιστοσελίδες που χρησιμοποιούν αυτό το βιβλίο</a></b>
+        </p>
+        <p>
+                Μεταφράσεις του Βιβλίου:
+        </p>
+        <ul>
+                <li>
+                        Αγγλικά - <a href="https://amzn.to/29w0lsX" target="_blank">Python for Everbody: Exploring Data in Python3</a>
+                        <ul class="menu vertical nested is-active">
+                               <li class="menu-text">
+                                       Τυπωμένο βιβλίο στο <a href="https://www.amazon.in/dp/9352136276" target="_blank">Amazon India</a>
+                                       (χαμηλού κόστους αποστολές εντός της Ινδίας χάρη στο 
+                                       <a href="http://www.shroffpublishers.com/books/9789352136278/" target="_blank">Shroff Publishing</a>)
+                                </li>
+                                <li class="menu-text"><a href="http://amzn.to/29LrtC0" target="_blank">Έκδοση Kindle</a> of the book</li>
+                                <li class="menu-text">Δωρεάν: <a href="http://do1.dr-chuck.com/pythonlearn/EN_us/pythonlearn.pdf" target="_blank">PDF</a>,
+                                        <a href="html3">HTML</a>, <a href="http://do1.dr-chuck.com/pythonlearn/EN_us/pythonlearn.epub" target="_blank">EPUB</a>
+                                </li>
+                                <li class="menu-text"><a href="https://eng.libretexts.org/Textbook_Maps/Computer_Science/Map%3A_Python_for_Everybody_(Severance)" target="_blank">HTML με παραδείγματα σε Jupyter Notebooks από LibreTexts.org</a></li>
 <!--
        <li class="menu-text"><a href="https://github.com/csev/py4e/raw/master/book3/zips/pfe.zip" target="_blank">Download HTML in a ZIP (~6MB)</a></li>
 -->
 <!--
        <li class="menu-text"><a href="trinket3" target="_blank">Interactive HTML from Trinket.io</a></li>
 -->
-       <li class="menu-text"><a href="https://books.trinket.io/pfe/index.html" target="_blank">Interactive HTML from Trinket.io</a></li>
-     </ul>
-  </li>
-<li> Spanish -
-<a href="https://amzn.to/3axKzHv" target="_blank">Python para todos: Explorando la información con Python 3</a>
-<ul>
-<li>
-Translated book, autograders, resources, and web site at
-<a href="https://es.py4e.com/book">https://es.py4e.com</a>
-</li>
-<li>
-<a href="https://github.com/csev-es" target="_blank">Github repo</a> -
-    Contributors:
-    <a href="https://github.com/don-charlie-pc" target="_blank">Juan Carlos Perez Castellanos</a>,
-    <a href="https://github.com/jdougnac" target="_blank">Juan Dougnac</a>,
-    <a href="https://github.com/danielmerino" target="_blank">Daniel Merino Echeverría</a>,
-    Jaime Bermeo Ramírez
-    and
-    <a href="https://github.com/ftardio" target="_new">Fernando Tardío</a>.
-</li>
-</ul>
-</li>
-<li> Italian
-<a href="https://amzn.to/2qGksK6" target="_blank">Python per tutti: Esplorare dati con Python3</a>
-     <ul class="menu vertical nested is-active">
-<li>
-       Free <a href="http://do1.dr-chuck.com/pythonlearn/IT_it/pythonlearn.pdf" target="_blank">PDF</a>,
-       <a href="http://do1.dr-chuck.com/pythonlearn/IT_it/pythonlearn.epub" target="_blank">EPUB</a>
-</li>
-<li>
-<a href="https://github.com/vittore/py4e/" target="_blank">Book source on github</a>,
-(Thanks to
-<a href="https://www.linkedin.com/in/alessandrorossetti/" target="_blank">Alessandro Rossetti</a>
-and
-<a href="https://www.linkedin.com/in/vittorezen/" target="_blank">Vittore Zen</a>)
-</li>
-     </ul>
-  </li>
-  <li> Portuguese -
-<a href="https://amzn.to/2S6GpA6" target="_blank">Python Para Todos: Explorando Dados com Python 3</a>
-     <ul class="menu vertical nested is-active">
-       <li class="menu-text">Free:
-        <a href="http://do1.dr-chuck.com/pythonlearn/PT_br/pythonlearn.pdf" target="_blank">PDF</a>,
-       <a href="http://do1.dr-chuck.com/pythonlearn/PT_br/pythonlearn.epub" target="_blank">EPUB</a>
-        </li>
-        <li> <a href="https://github.com/ras-ufcg/py4e" target="_blank">Book source on github</a> and
-<a href="https://github.com/ras-ufcg/py4e/blob/master/RAS_Team_Members.txt" target="_blank">translation team</a>
-(Thanks to <a href="https://twitter.com/yuriloia" target="_blank">Yuri Loia de Medeiros</a>).
-</ul>
-</li>
-<li>
-Polish -
-<a href="https://amzn.to/38M2zjt" target="_blank">Python dla wszystkich: Odkrywanie danych z Python 3</a>
-(<a href="https://www.amazon.pl/gp/product/8396017603" target="_blank">from Amazon.pl</a> and
-<a href="https://www.amazon.de/gp/product/8396017603" target="_blank">from Amazon.de</a>)
-     <ul class="menu vertical nested is-active">
-<li>
-Translated book, autograders, resources, and web site at
-<a href="https://py4e.pl/" target="blank">https://py4e.pl</a>
-</li>
-            <li class="menu-text">
-                <a href="https://py4e.pl/html3">HTML</a>,
-     <a href="https://py4e.pl/translations/PL/py4e-pl-a4-latest.pdf" target="_blank">PDF (format A4)</a>,
-<a href="https://py4e.pl/translations/PL/py4e-pl-print-latest.pdf" target="_blank">PDF (format for print)</a>,
-<a href="https://py4e.pl/translations/PL/py4e-pl-latest.epub" target="_blank">EPUB</a>,
-<a href="https://py4e.pl/translations/PL/py4e-pl-latest.mobi" target="_blank">MOBI</a>
-            </li>
-            <li class="menu-text"><a href="https://github.com/andre-wojtowicz/py4e-pl" target="_blank">Github Repository</a>
-        <li>Contributors: <a href="https://github.com/andre-wojtowicz" target="_blank">Andrzej Wójtowicz</a> (Adam Mickiewicz University in Poznań, Poland)</li>
-    </li>
-</ul>
-</li>
-  <li> Korean (commercial translation)
-<a href="http://www.yes24.com/Product/Goods/77401048?Acode=101" target="_blank">데이터를 다루며 배우는 파이썬</a>
-  </li>
-  </li>
-  <li>
-     Chinese - In progress
-     <ul class="menu vertical nested is-active">
-       <li class="menu-text">Free:
+                                <li class="menu-text"><a href="https://books.trinket.io/pfe/index.html" target="_blank">Διαδραστικό HTML από Trinket.io</a></li>
+                        </ul>
+                </li>
+                <li> Ισπανικά - <a href="https://amzn.to/3axKzHv" target="_blank">Python para todos: Explorando la información con Python 3</a>
+                        <ul>
+                                <li>
+                                        Μεταφρασμένο βιβλίο, βαθμολογητές, πόροι και ιστότοπος στη διεύθυνση <a href="https://es.py4e.com/book">https://es.py4e.com</a>
+                                </li>
+                                <li>
+                                        <a href="https://github.com/csev-es" target="_blank">Github repo</a> - Contributors:
+                                        <a href="https://github.com/don-charlie-pc" target="_blank">Juan Carlos Perez Castellanos</a>,
+                                        <a href="https://github.com/jdougnac" target="_blank">Juan Dougnac</a>,
+                                        <a href="https://github.com/danielmerino" target="_blank">Daniel Merino Echeverría</a>,
+                                        Jaime Bermeo Ramírez και <a href="https://github.com/ftardio" target="_new">Fernando Tardío</a>.
+                                </li>
+                        </ul>
+                </li>
+                <li> Ιταλικά - <a href="https://amzn.to/2qGksK6" target="_blank">Python per tutti: Esplorare dati con Python3</a>
+                        <ul class="menu vertical nested is-active">
+                                <li>
+                                        Δωρεάν <a href="http://do1.dr-chuck.com/pythonlearn/IT_it/pythonlearn.pdf" target="_blank">PDF</a>,
+                                       <a href="http://do1.dr-chuck.com/pythonlearn/IT_it/pythonlearn.epub" target="_blank">EPUB</a>
+                                </li>
+                                <li>
+                                        <a href="https://github.com/vittore/py4e/" target="_blank">Ο κώδικάς του βιβλίου στο github</a>,
+                                        (Χάρη στους <a href="https://www.linkedin.com/in/alessandrorossetti/" target="_blank">Alessandro Rossetti</a>
+                                        και <a href="https://www.linkedin.com/in/vittorezen/" target="_blank">Vittore Zen</a>)
+                                </li>
+                        </ul>
+                </li>
+                <li> Πορτογαλικά - <a href="https://amzn.to/2S6GpA6" target="_blank">Python Para Todos: Explorando Dados com Python 3</a>
+                        <ul class="menu vertical nested is-active">
+                                <li class="menu-text">Δωρεάν:
+                                        <a href="http://do1.dr-chuck.com/pythonlearn/PT_br/pythonlearn.pdf" target="_blank">PDF</a>,
+                                       <a href="http://do1.dr-chuck.com/pythonlearn/PT_br/pythonlearn.epub" target="_blank">EPUB</a>
+                                </li>
+                                <li> 
+                                        <a href="https://github.com/ras-ufcg/py4e" target="_blank">Ο κώδικάς του βιβλίου στο github</a> and
+                                        <a href="https://github.com/ras-ufcg/py4e/blob/master/RAS_Team_Members.txt" target="_blank">μεταφραστική ομάδα</a>
+                                        (Χάρη σε <a href="https://twitter.com/yuriloia" target="_blank">Yuri Loia de Medeiros</a>).
+                                </li>                                        
+                        </ul>
+                </li>
+                <li>
+                        Πολωνικά - <a href="https://amzn.to/38M2zjt" target="_blank">Python dla wszystkich: Odkrywanie danych z Python 3</a>
+                        (<a href="https://www.amazon.pl/gp/product/8396017603" target="_blank">from Amazon.pl</a> and
+                        <a href="https://www.amazon.de/gp/product/8396017603" target="_blank">from Amazon.de</a>)
+                        <ul class="menu vertical nested is-active">
+                                <li>
+                                        Μεταφρασμένο βιβλίο, βαθμολογητές, πόροι και ιστότοπος στη διεύθυνση <a href="https://py4e.pl/" target="blank">https://py4e.pl</a>
+                                </li>
+                                <li class="menu-text">
+                                        <a href="https://py4e.pl/html3">HTML</a>,
+                                        <a href="https://py4e.pl/translations/PL/py4e-pl-a4-latest.pdf" target="_blank">PDF (μορφή A4)</a>,
+                                        <a href="https://py4e.pl/translations/PL/py4e-pl-print-latest.pdf" target="_blank">PDF (μορφή για εκτύπωση)</a>,
+                                        <a href="https://py4e.pl/translations/PL/py4e-pl-latest.epub" target="_blank">EPUB</a>,
+                                        <a href="https://py4e.pl/translations/PL/py4e-pl-latest.mobi" target="_blank">MOBI</a>
+                                </li>
+                                <li class="menu-text"><a href="https://github.com/andre-wojtowicz/py4e-pl" target="_blank">Github Repository</a> /li>
+                                <li>
+                                        Contributors: <a href="https://github.com/andre-wojtowicz" target="_blank">Andrzej Wójtowicz</a> 
+                                        (Adam Mickiewicz University in Poznań, Poland)</li>
+                        </ul>
+                </li>
+                <li> Κορεατικά (εμπορική μετάφραση)<a href="http://www.yes24.com/Product/Goods/77401048?Acode=101" target="_blank">데이터를 다루며 배우는 파이썬</a></li>
+                <li> Κινέζικα - Σε εξέλιξη
+                        <ul class="menu vertical nested is-active">
+                                <li class="menu-text">Δωρεάν:
+                                        <a href="http://do1.dr-chuck.com/pythonlearn/ZH_cn/pythonlearn.pdf" target="_blank">PDF</a>,
+                                        <a href="http://do1.dr-chuck.com/pythonlearn/ZH_cn/pythonlearn.epub" target="_blank">EPUB</a>
+                                </li>
+                                <li> <a href="https://github.com/RodenLuo/py4e-cn" target="_blank">Ο κώδικάς του βιβλίου στο  github</a> - (Χάρη σε Deng Luo)</li>
+                                <li> Υπάρχει ένα αντίγραφο στην Κίνα των βίντεο διαθέσιμο στη διεύθυνση 
+                                        <a href="https://www.bilibili.com/video/av46649799" target="_new">https://www.bilibili.com/video/av46649799</a>
+                                </li>
+                        </ul>
+                </li>
+                <li>Ρωσικά - Σε εξέλιξη
+                        <ul>
+                                <li>
+                                        <a href="https://dmkpress.com" target="_blank">DMK Press</a>
+                                        εργάζεται για μια εμπορική μετάφραση του βιβλίου και θα παράσχει δωρεάν PDF σε αυτόν τον ιστότοπο όταν ολοκληρωθεί.
+                                </li>
+                                <li>
+                                        <a href="lectures3/ru/index.htm" target="_blank">Ρωσική έκδοση των διαφανειών</a> από την
+                                        <a href="https://www.linkedin.com/in/violetta-fomkina-9451372/" target="_blank">Violetta Fomkina</a>.
+                                </li>
+                        </ul>
 
-
-        <a href="http://do1.dr-chuck.com/pythonlearn/ZH_cn/pythonlearn.pdf" target="_blank">PDF</a>,
-       <a href="http://do1.dr-chuck.com/pythonlearn/ZH_cn/pythonlearn.epub" target="_blank">EPUB</a>
-        </li>
-        <li>
-<a href="https://github.com/RodenLuo/py4e-cn" target="_blank">Book source on github</a> -
-(Thanks to Deng Luo)
-</li>
-  <li> There is an in-China copy of the videos available at
-<a href="https://www.bilibili.com/video/av46649799" target="_new">
-https://www.bilibili.com/video/av46649799</a>
-  </li>
-     </ul>
-  </li>
-  <li>
-    Russian - In progress
-    <ul>
-<li><a href="https://dmkpress.com" target="_blank">DMK Press</a>
-is working on a commercial translation of the book and will provide a free PDF for this site when it is completed.</li>
-    <li><a href="lectures3/ru/index.htm" target="_blank">Russian versions of the slides</a> from
-<a href="https://www.linkedin.com/in/violetta-fomkina-9451372/" target="_blank">Violetta Fomkina</a>.
-</li>
-</ul>
-
-<li>
-Greek - In progress
-<ul>
-<li>
-<a href="lectures3/gr" target="_blank">Greek versions of the slides</a> from 
-<a href="https://twitter.com/KonstantiaQ" target="_blank">Konstantia Kirourtidou</a></li>
-</ul>
-</li>
-
+                <li>Ελληνικά - Σε εξέλιξη
+                        <ul>
+                                <li>
+                                        <a href="lectures3/gr" target="_blank">Ελληνική έκδοση των διαφανειών</a> από την 
+                                        <a href="https://twitter.com/KonstantiaQ" target="_blank">Konstantia Kirourtidou</a>
+                                </li>
+                        </ul>
+                </li>
+        </ul>
 <p>
-If you are insterested in starting a translation of the book, I have some
-instructions for
-<a href="https://github.com/csev/py4e/blob/master/TRANSLATION.md"
- target="_blank">
-getting started</a>.
+        Εάν ενδιαφέρεστε να ξεκινήσετε μια μετάφραση του βιβλίου, έχω μερικές οδηγίες για 
+        <a href="https://github.com/csev/py4e/blob/master/TRANSLATION.md" target="_blank">να ξεκινήσετε</a>.
 </p>
-        <p>
-            Chapters 2-10 are heavily adapted from the open book titled: "<a href="http://www.greenteapress.com/thinkpython/thinkCSpy/" target="_blank">Think Python: How to Think like a Computer Scientist</a>" by <a href="http://allendowney.com/" target="_blank">Allen B. Downey</a>
-and <a href="http://www.elkner.net/" target="_blank">Jeff Elkner</a>.
-The <a href="http://www.py4inf.com/">Python 2 version</a> of the book is still available.
-        </p>
-</div>
-</div>
+<p>
+        Τα κεφάλαια 2-10 είναι σε μεγάλο βαθμό προσαρμοσμένα από το ανοιχτό βιβλίο με τίτλο: 
+        "<a href="http://www.greenteapress.com/thinkpython/thinkCSpy/" target="_blank">Think Python: How to Think like a Computer Scientist</a>" 
+        των <a href="http://allendowney.com/" target="_blank">Allen B. Downey</a> και <a href="http://www.elkner.net/" target="_blank">Jeff Elkner</a>.
+        Η <a href="http://www.py4inf.com/">έδοση Python 2</a> του βιβλίου είναι ακόμα διαθέσιμη.
+</p>
 <?php
 include("footer.php");
 ?>
