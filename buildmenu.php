36ce64190602a3c3fa9559b1e5a0a45e4de1ef6d
Charles Severance <csev@umich.edu>
2021-09-20 15:58:26 -0400

    Switch instructor link

--- buildmenu.php
+++ buildmenu.php
@@ -51,7 +51,7 @@ function buildMenu() {
 
     $imenu = new \Tsugi\UI\Menu();
 
-    $imenu->addLink('Instructor', 'http://www.dr-chuck.com');
+    $imenu->addLink('Instructor', 'http://online.dr-chuck.com');
     $imenu->addLink('Office Hours', 'http://www.dr-chuck.com/office/');
     $set->addRight('Book', $R . 'book');
     $set->addRight('Instructor', $imenu);
