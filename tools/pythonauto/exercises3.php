3adb3fbdb2341bc79844a479ddcb025357b0733b
konkiourtidou <dinakiourtidou1@gmail.com>
2021-09-22 11:34:42 +0300

    ex2.3

--- tools/pythonauto/exercises3.php
+++ tools/pythonauto/exercises3.php
@@ -66,7 +66,7 @@ print("Γειά σου ")',
 
 
 "3.1" => Array(
-"qtext" => "<b>3.1</b> Write a program to prompt the user for hours and rate per hour using input
+"qtext" => "<b>3.1</b> Γράψτε ένα πρόγραμμα που προτρέπει τον χρήστη for hours and rate per hour using input
 to compute gross pay.  Pay the hourly rate for the hours up to 40 and
 1.5 times the hourly rate for all hours
 worked above 40 hours.  Use 45 hours and a rate of 10.50 per hour to test the
