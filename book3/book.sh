6c284b36e93c5dd4664f2e76ad732abadf6b3933
konkiourtidou <dinakiourtidou1@gmail.com>
2022-09-01 00:56:06 +0300

    Update book.sh

--- book3/book.sh
+++ book3/book.sh
@@ -4,16 +4,15 @@
 #    touch .chinese
 # in this folder so as to run the LaTex to make Chinese output
 
-# For yucks make the epub
-cat epub-metadata.txt *.mkd | grep -v '^%' | python pre-html.py | python verbatim.py | pandoc --default-image-extension=svg --css=epub.css -o x.epub
-
-# make the mobi if it works (add verbose for debugging)
-if hash kindlegen 2>/dev/null; then
-    kindlegen x.epub 
-    echo "mobi generated"
-else
-    echo "mobi not generated - please install kindlegen"
+# Pick your Python
+MY_PYTHON=python
+if which python3; then
+MY_PYTHON=python3
 fi
+echo Using python command: $MY_PYTHON
+
+# For yucks make the epub
+cat epub-metadata.txt *.mkd | grep -v '^%' | $MY_PYTHON pre-html.py | $MY_PYTHON verbatim.py | iconv -f utf8 -t ascii//TRANSLIT | pandoc --default-image-extension=svg --css=epub.css -o x.epub
 
 rm tmp.* *.tmp *.aux
 pandoc A0-preface.mkd -o tmp.prefacex.tex
@@ -21,18 +20,19 @@ sed < tmp.prefacex.tex 's/section{/section*{/' > tmp.preface.tex
 # pandoc -s -N -f markdown+definition_lists -t latex --toc --default-image-extension=eps -V fontfamily:arev -V fontsize:10pt -V documentclass:book --template=template.latex [0-9]*.mkd [A][A-Z]*.mkd -o tmp.tex
 if [ -f .chinese ] ; then
     # pandoc -o c.pdf --latex-engine=xelatex -V mainfont='Adobe Ming Std' 01-intro.mkd
-    cat [0-9]*.mkd | python verbatim.py | tee tmp.verbatim | pandoc -s -N -f markdown+definition_lists -t latex --toc --default-image-extension=eps -V fontsize:10pt -V documentclass:book -V mainfont='Noto Serif CJK SC' --template=template.latex -o tmp.tex
+    cat [0-9]*.mkd | $MY_PYTHON verbatim.py | iconv -f utf8 -t ascii//TRANSLIT | tee tmp.verbatim | pandoc -s -N -f markdown+definition_lists -t latex --toc --default-image-extension=eps -V fontsize:10pt -V documentclass:book -V mainfont='Noto Serif CJK SC' --template=template.latex -o tmp.tex
     pandoc -V mainfont='Noto Serif CJK SC' [A-Z][A-Z]*.mkd -o tmp.app.tex
 else
-    cat [0-9]*.mkd | python verbatim.py | tee tmp.verbatim | pandoc -s -N -f markdown+definition_lists -t latex --toc --default-image-extension=eps -V fontsize:10pt -V documentclass:book --template=template.latex -o tmp.tex
+    cat [0-9]*.mkd | $MY_PYTHON verbatim.py | iconv -f utf8 -t ascii//TRANSLIT > tmp.verbatim 
+    pandoc -s -N -f markdown+definition_lists -t latex --toc --default-image-extension=eps -V fontsize:10pt -V documentclass:book --template=template.latex -o tmp.tex < tmp.verbatim 
     pandoc [A-Z][A-Z]*.mkd -o tmp.app.tex
 fi
 
 sed < tmp.app.tex -e 's/subsubsection{/xyzzy{/' -e 's/subsection{/plugh{/' -e 's/section{/chapter{/' -e 's/xyzzy{/subsection{/' -e 's/plugh{/section{/'  > tmp.appendix.tex
 
-sed < tmp.tex '/includegraphics/s/jpg/eps/' | sed 's"includegraphics{../photos"includegraphics[height=3.0in]{../photos"' > tmp.sed
+sed < tmp.tex '/includegraphics/s/jpg/eps/' | sed 's"includegraphics{../photos"includegraphics[height=3.0in]{../photos"' | iconv -f utf8 -t ascii//TRANSLIT > tmp.sed
 diff tmp.sed tmp.tex
-python texpatch.py < tmp.sed > tmp.patch
+$MY_PYTHON texpatch.py < tmp.sed | iconv -f utf8 -t ascii//TRANSLIT > tmp.patch
 
 mv tmp.patch tmp.tex
 if [ -f .chinese ] ; then
