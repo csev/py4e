2c46830a20183328501242c130f8390f8b5d3616
Charles Severance <csev@umich.edu>
2022-12-01 16:28:49 -0500

    From Konstantia

---
+++ editors.php
@@ -0,0 +1,33 @@
+<?php include("top.php");?>
+<?php include("nav.php");?>
+<div class="hide-for-large" id="mobile-support"></div>
+<div id="first-row" class="row">
+    <div class="small-12 columns">
+        <h2>Recommended Programming Text Editors</h2>
+        <p>
+            We have separate pages for each of the commonly used Systems:
+        </p>
+        <ul>
+            <li>
+                <a href="https://brackets.io/">The Brackets programmer text editor</a> 
+                    Basic
+            </li>
+            <li>
+                <a href="https://code.visualstudio.com/">The VSCode programmer text editor</a> 
+                    Advanced
+            </li>
+            <li>
+                <a href="https://www.jetbrains.com/pycharm/">PyCharm</a>
+                    interactive development environment (IDE) [note: you <b>must</b> apply to JetBrain's for a free <a href="https://www.jetbrains.com/student/">student license</a>]
+            </li>
+            <li>
+                <a href="http://www.sublimetext.com/">Sublime programmer text editor</a>
+            </li>
+        </ul>
+        <p>
+            and many others, depending on your programming experience and knowledge. Choice is yours.
+        </p>
+
+    </div>
+</div>
+<?php include("footer.php"); ?>
