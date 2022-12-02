2ca197f0b310275d163a2ce7db69c046f4541e89
konkiourtidou <dinakiourtidou1@gmail.com>
2022-12-02 12:11:56 +0200

    update

---
+++ editors.php
@@ -0,0 +1,33 @@
+<?php include("top.php");?>
+<?php include("nav.php");?>
+<div class="hide-for-large" id="mobile-support"></div>
+<div id="first-row" class="row">
+    <div class="small-12 columns">
+        <h2>Προτεινόμενοι Επεξεργαστές Κειμένου Προγραμματισμού</h2>
+        <p>
+            Έχουμε ξεχωριστές σελίδες για καθένα από τα Συστήματα που χρησιμοποιούνται συνήθως:
+        </p>
+        <ul>
+            <li>
+                <a href="https://brackets.io/">Ο Brackets programmer text editor</a> 
+                    Απλός
+            </li>
+            <li>
+                <a href="https://code.visualstudio.com/">Ο VSCode programmer text editor</a> 
+                    Για προχωρημένους
+            </li>
+            <li>
+                <a href="https://www.jetbrains.com/pycharm/">PyCharm</a>
+                    διαδραστικό περιβάλλον ανάπτυξης (IDE) [Σημείωση: <b>πρέπει</b> να κάντε αίτηση στο JetBrain's για δωρεάν <a href="https://www.jetbrains.com/student/">student license (φοιτητική άδεια)</a>]
+            </li>
+            <li>
+                <a href="http://www.sublimetext.com/">Sublime programmer text editor</a>
+            </li>
+        </ul>
+        <p>
+            και πολλοί άλλοι, ανάλογα με την προγραμματιστική εμπειρία και τις γνώσεις σας. Η επιλογή είναι δική σας.
+        </p>
+
+    </div>
+</div>
+<?php include("footer.php"); ?>
