fc05d08c127d60e641e3abd3a48c4bc049b3a57c
Charles Severance <csev@umich.edu>
2022-12-19 10:49:16 -0500

    Updates

--- editors.php
+++ editors.php
@@ -3,31 +3,51 @@
 <div class="hide-for-large" id="mobile-support"></div>
 <div id="first-row" class="row">
     <div class="small-12 columns">
-        <h2>Recommended Programming Text Editors</h2>
-        <p>
-            We have separate pages for each of the commonly used Systems:
-        </p>
+        <h1>Programming Text Editor</h1>
+<p>
+You will need a text editor to use to write programs.  In particular, there are
+number of editors you <b>should not use</b> for programming including, Microsoft Word,
+NotePad, TextEdit or Libre Office.  These editors tend not to store "plain text" which
+is how we need to write programs.   Programs cannot have things like bold, italics, or different
+fonts - and even if you don't use these things in your file, the files are stored in a format
+that can represent all this "pretty" text stuff - and that confuses compilers and interpreters.
+</p>
+<p>
+If you already have an editor or development environment installed please <b>keep using it</b>.
+There is no need to install another editor just for this course.
+</p>
+<h2>Recommended Programmer Editor: Brackets.IO</h2>
+<p>
+If you are installing your first programmer editor, we recommend you keep it simple
+and use 
+<a href="https://brackets.io/" target="_blank">brackets.io</a> - it 
+is free, open source, and works on Windows, Linux, and Macintosh.
         <ul>
             <li>
-                <a href="https://brackets.io/">The Brackets programmer text editor</a> 
-                    Basic
+                <a href="https://brackets.io/" target="_blank">The Brackets programmer text editor</a> 
+            </li>
+</ul>
+</p>
+<h2>Alternate Programmer Editors</h2>
+<p>
+There are more sophisticated text editors and integrated development environments available.  These
+are often more complex to install - but can greatly improve your productivity.
+<ul>
+            <li>
+                <a href="https://code.visualstudio.com/" target="_blank">The VSCode programmer text editor</a> 
             </li>
             <li>
-                <a href="https://code.visualstudio.com/">The VSCode programmer text editor</a> 
-                    Advanced
-            </li>
-            <li>
-                <a href="https://www.jetbrains.com/pycharm/">PyCharm</a>
+                <a href="https://www.jetbrains.com/pycharm/" target="_blank">PyCharm</a>
                     interactive development environment (IDE) [note: you <b>must</b> apply to JetBrain's for a free <a href="https://www.jetbrains.com/student/">student license</a>]
             </li>
             <li>
-                <a href="http://www.sublimetext.com/">Sublime programmer text editor</a>
+                <a href="http://www.sublimetext.com/" target="_blank">Sublime programmer text editor</a>
             </li>
         </ul>
-        <p>
-            and many others, depending on your programming experience and knowledge. Choice is yours.
-        </p>
-
+</p>
+<p>
+You can use any programmer editor you like for this course.
+</p>
     </div>
 </div>
 <?php include("footer.php"); ?>
