45f5ad0820809c89ee7df5b4352eb1c6f3cd1f3c
Charles Severance <csev@umich.edu>
2023-12-30 19:33:57 -0500

    Progress on Chapter 15

--- code3/tracks/README.txt
+++ code3/tracks/README.txt
@@ -1,6 +1,17 @@
-To export your own Library.xml from iTunes 
+
+There are two versions of the tracks application:
+
+-  tracks_csv.py - Reads the tracks.csv file
+
+-  tracks.py Reads and parses the Library.xml file which is exported from iTunes
+
+The tracks_csv.py file is simpler and easier to understand and focuses on the
+database learning objectives of this sample code.
+
+If you are using the original tracks.py that is reading XML, you can 
+export your own Library.xml from iTunes 
 
 File -> Library -> Export Library
 
-Make sure it is in the correct folder.   Of course iTUnes might change
+Make sure it is in the correct folder.   Of course iTunes might change
 UI and/or export format any time - so good luck :)
---
+++ code3/tracks/tracks_csv.py
@@ -0,0 +1,66 @@
+import sqlite3
+
+conn = sqlite3.connect('trackdb.sqlite')
+cur = conn.cursor()
+
+# Make some fresh tables using executescript()
+cur.executescript('''
+DROP TABLE IF EXISTS Artist;
+DROP TABLE IF EXISTS Album;
+DROP TABLE IF EXISTS Track;
+
+CREATE TABLE Artist (
+    id  INTEGER PRIMARY KEY,
+    name    TEXT UNIQUE
+);
+
+CREATE TABLE Album (
+    id  INTEGER PRIMARY KEY,
+    artist_id  INTEGER,
+    title   TEXT UNIQUE
+);
+
+CREATE TABLE Track (
+    id  INTEGER PRIMARY KEY,
+    title TEXT  UNIQUE,
+    album_id  INTEGER,
+    len INTEGER, rating INTEGER, count INTEGER
+);
+''')
+
+
+handle = open('tracks.csv')
+
+# Another One Bites The Dust,Queen,Greatest Hits,55,100,217103
+#   0                          1      2           3  4   5
+
+for line in handle:
+    line = line.strip();
+    pieces = line.split(',')
+    if len(pieces) != 6 : continue
+
+    name = pieces[0]
+    artist = pieces[1]
+    album = pieces[2]
+    count = pieces[3]
+    rating = pieces[4]
+    length = pieces[5]
+
+    print(name, artist, album, count, rating, length)
+
+    cur.execute('''INSERT OR IGNORE INTO Artist (name) 
+        VALUES ( ? )''', ( artist, ) )
+    cur.execute('SELECT id FROM Artist WHERE name = ? ', (artist, ))
+    artist_id = cur.fetchone()[0]
+
+    cur.execute('''INSERT OR IGNORE INTO Album (title, artist_id) 
+        VALUES ( ?, ? )''', ( album, artist_id ) )
+    cur.execute('SELECT id FROM Album WHERE title = ? ', (album, ))
+    album_id = cur.fetchone()[0]
+
+    cur.execute('''INSERT OR REPLACE INTO Track
+        (title, album_id, len, rating, count) 
+        VALUES ( ?, ?, ?, ?, ? )''', 
+        ( name, album_id, length, rating, count ) )
+
+    conn.commit()
