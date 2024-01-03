db7fc2c8197dd388f8100f0ff84d9cceea510dc7
Charles Severance <csev@umich.edu>
2024-01-02 23:46:34 -0500

    Fix for issue #245

--- code3/count2.py
+++ code3/count2.py
@@ -10,7 +10,8 @@ except:
 counts = dict()
 for line in fhand:
     line = line.rstrip()
-    line = line.translate(line.maketrans('', '', string.punctuation))
+    # First two parameters are empty strings
+    line = line.translate(line.maketrans("", "", string.punctuation))
     line = line.lower()
     words = line.split()
     for word in words:
