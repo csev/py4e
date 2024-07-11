09b006e383772dbca508b134344dd15222a2dc96
Charles Severance <csev@umich.edu>
2024-07-11 11:32:40 -0400

    Per Tamara

--- code3/xml3.py
+++ code3/xml3.py
@@ -3,7 +3,7 @@ import xml.etree.ElementTree as ET
 
 url = input('Enter location: ')
 if len(url) < 1 : 
-    url = 'https://py4e-data.dr-chuck.net/comments_42.xml'
+    url = 'http://py4e-data.dr-chuck.net/comments_42.xml'
 
 print('Retrieving', url)
 uh = urllib.request.urlopen(url)
