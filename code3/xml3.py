68696c482d9a79bfdafdc7573f8520c1a9941a8f
Charles Severance <csev@umich.edu>
2024-07-11 06:53:51 -0400

    Add new file.

---
+++ code3/xml3.py
@@ -0,0 +1,22 @@
+import urllib.request
+import xml.etree.ElementTree as ET
+
+url = input('Enter location: ')
+if len(url) < 1 : 
+    url = 'https://py4e-data.dr-chuck.net/comments_42.xml'
+
+print('Retrieving', url)
+uh = urllib.request.urlopen(url)
+data = uh.read()
+print('Retrieved',len(data),'characters')
+tree = ET.fromstring(data)
+
+counts = tree.findall('.//count')
+nums = list()
+for result in counts:
+    # Debug print the data :)
+    print(result.text)
+
+print('Count:', len(nums))
+print('Sum:', sum(nums))
+
