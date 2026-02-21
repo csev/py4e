901ea220b0efc38368e439fd9d992aad58cf81bc
Charles Severance <csev@umich.edu>
2026-02-21 10:55:46 -0500

    Updates

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
 <?php
 
 $foot = '
-<p style="font-size: 75%; margin-top: 5em;">
-Copyright Creative Commons Attribution 3.0 - Charles R. Severance
+<p style="font-size: 0.875rem; color: #333; margin-top: 5em;">
+Copyright Creative Commons Attribution 4.0 - Charles R. Severance
 </p><script type="module" src="' . htmlspecialchars($CFG->wwwroot.'/lms/announce/tsugi-announce.js') .'"></script>';
 
 $OUTPUT->setAppFooter($foot);
