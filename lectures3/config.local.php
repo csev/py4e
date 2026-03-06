8a6214a22ff76dfa42fe9a20067e2a68cc288948
Charles Severance <csev@umich.edu>
2026-03-06 09:14:11 -0500

    Allow Lecture listing.

---
+++ lectures3/config.local.php
@@ -0,0 +1,7 @@
+<?php
+// Local configuration overrides
+$PASSWORD_PROTECTION_ENABLED = false;
+$SITE_PASSWORD = '42';
+$HOME_URL = 'https://www.py4e.com';
+$PAGE_TITLE = "Python For Everybody Lectures";
+
