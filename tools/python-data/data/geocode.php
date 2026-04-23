818541b390040591fadef8dd4a3f8da60f3bb435
Charles Severance <csev@umich.edu>
2026-04-23 10:13:15 -0400

    Deptecate the whol Google thing :)

--- tools/python-data/data/geocode.php
+++ tools/python-data/data/geocode.php
@@ -1,5 +1,26 @@
 <?php
 
+$do_json = strpos($_SERVER['REQUEST_URI'],'/xml') === false;
+
+if ( $do_json ) {
+    header('Content-Type: application/json');
+    echo('{"error": "This Google-based endpoint is no longer supported. We are now using OpenGeo in this course.",'."\n");
+    echo('"new_sample_code": "https://www.py4e.com/code3/?folder=opengeo"}'."\n");
+    return;
+} else {
+    header('Content-Type: application/xml');
+    echo('<?xml version="1.0" encoding="UTF-8"?>'."\n");
+?>
+<root>
+  <error>This Google-based endpoint is no longer supported. We are now using OpenGeo in this course.</error>
+  <new_sample_code>https://www.py4e.com/code3/?folder=opengeo</new_sample_code>
+</root>
+<?php
+    return;
+}
+
+// Old code
+
 require_once "rate_limit.php";
 
 if ( ! isset($CFG->GEOCODE_KEY) ) {
